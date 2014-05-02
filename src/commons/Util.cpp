1dca2e6d9d3a26cc32c54d12cfd1fbf167a074eb
Martin Steinegger <mad@dhcp-138-246-124-119.dynamic.eduroam.mwn.de>
2014-05-02 17:05:45 +0200

    Improve performance SORT20

--- src/commons/Util.cpp
+++ src/commons/Util.cpp
@@ -63,7 +63,7 @@ void Util::decomposeDomainByAminoaAcid(int aaSize, unsigned short * seqSizes, si
 }
 
 
-// http://jgamble.ripco.net/cgi-bin/nw.cgi?inputs=20&algorithm=bosenelson&output=svg
+// http://jgamble.ripco.net/cgi-bin/nw.cgi?inputs=20&algorithm=batcher&output=svg
 // // sorting networks
 void Util::rankedDescSort20(short * val, int * index){
 #define SWAP(x,y){\
@@ -76,27 +76,20 @@ void Util::rankedDescSort20(short * val, int * index){
         index[y] = tmp;      \
     } \
 }
-    SWAP(0,1);SWAP(3,4);SWAP(5,6);SWAP(8,9);SWAP(10,11);SWAP(13,14);SWAP(15,16);SWAP(18,19);
-    SWAP(2,4);SWAP(7,9);SWAP(12,14);SWAP(17,19);
-    SWAP(2,3);SWAP(1,4);SWAP(7,8);SWAP(6,9);SWAP(12,13);SWAP(11,14);SWAP(17,18);SWAP(16,19);
-    SWAP(0,3);SWAP(5,8);SWAP(4,9);SWAP(10,13);SWAP(15,18);SWAP(14,19);
-    SWAP(0,2);SWAP(1,3);SWAP(5,7);SWAP(6,8);SWAP(10,12);SWAP(11,13);SWAP(15,17);SWAP(16,18);SWAP(9,19);
-    SWAP(1,2);SWAP(6,7);SWAP(0,5);SWAP(3,8);SWAP(11,12);SWAP(16,17);SWAP(10,15);SWAP(13,18);
-    SWAP(1,6);SWAP(2,7);SWAP(4,8);SWAP(11,16);SWAP(12,17);SWAP(14,18);SWAP(0,10);
-    SWAP(1,5);SWAP(3,7);SWAP(11,15);SWAP(13,17);SWAP(8,18);
-    SWAP(4,7);SWAP(2,5);SWAP(3,6);SWAP(14,17);SWAP(12,15);SWAP(13,16);SWAP(1,11);SWAP(9,18);
-    SWAP(4,6);SWAP(3,5);SWAP(14,16);SWAP(13,15);SWAP(1,10);SWAP(2,12);SWAP(7,17);
-    SWAP(4,5);SWAP(14,15);SWAP(3,13);SWAP(2,10);SWAP(6,16);SWAP(8,17);
-    SWAP(4,14);SWAP(3,12);SWAP(5,15);SWAP(9,17);SWAP(8,16);
-    SWAP(4,13);SWAP(3,11);SWAP(6,15);SWAP(9,16);
-    SWAP(4,12);SWAP(3,10);SWAP(7,15);
-    SWAP(4,11);SWAP(8,15);SWAP(7,12);
-    SWAP(4,10);SWAP(9,15);SWAP(6,11);SWAP(8,13);
-    SWAP(5,10);SWAP(9,14);SWAP(8,12);
-    SWAP(6,10);SWAP(9,13);SWAP(8,11);
-    SWAP(9,12);SWAP(7,10);
-    SWAP(9,11);SWAP(8,10);
-    SWAP(9,10);
+    SWAP(0,16);SWAP(1,17);SWAP(2,18);SWAP(3,19);SWAP(4,12);SWAP(5,13);SWAP(6,14);SWAP(7,15);
+    SWAP(0,8);SWAP(1,9);SWAP(2,10);SWAP(3,11);
+    SWAP(8,16);SWAP(9,17);SWAP(10,18);SWAP(11,19);SWAP(0,4);SWAP(1,5);SWAP(2,6);SWAP(3,7);
+    SWAP(8,12);SWAP(9,13);SWAP(10,14);SWAP(11,15);SWAP(4,16);SWAP(5,17);SWAP(6,18);SWAP(7,19);SWAP(0,2);SWAP(1,3);
+    SWAP(4,8);SWAP(5,9);SWAP(6,10);SWAP(7,11);SWAP(12,16);SWAP(13,17);SWAP(14,18);SWAP(15,19);SWAP(0,1);
+    SWAP(4,6);SWAP(5,7);SWAP(8,10);SWAP(9,11);SWAP(12,14);SWAP(13,15);SWAP(16,18);SWAP(17,19);
+    SWAP(2,16);SWAP(3,17);SWAP(6,12);SWAP(7,13);SWAP(18,19);
+    SWAP(2,8);SWAP(3,9);SWAP(10,16);SWAP(11,17);
+    SWAP(2,4);SWAP(3,5);SWAP(6,8);SWAP(7,9);SWAP(10,12);SWAP(11,13);SWAP(14,16);SWAP(15,17);
+    SWAP(2,3);SWAP(4,5);SWAP(6,7);SWAP(8,9);SWAP(10,11);SWAP(12,13);SWAP(14,15);SWAP(16,17);
+    SWAP(1,16);SWAP(3,18);SWAP(5,12);SWAP(7,14);
+    SWAP(1,8);SWAP(3,10);SWAP(9,16);SWAP(11,18);
+    SWAP(1,4);SWAP(3,6);SWAP(5,8);SWAP(7,10);SWAP(9,12);SWAP(11,14);SWAP(13,16);SWAP(15,18);
+    SWAP(1,2);SWAP(3,4);SWAP(5,6);SWAP(7,8);SWAP(9,10);SWAP(11,12);SWAP(13,14);SWAP(15,16);SWAP(17,18);
 #undef SWAP
 #undef min
 #undef max
