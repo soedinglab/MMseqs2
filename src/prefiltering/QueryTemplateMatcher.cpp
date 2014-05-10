49b21a0b3d4a686450d72c60b4df92f92f1eeacb
Maria Hauser <mhauser@genzentrum.lmu.de>
2014-05-10 19:04:58 +0200

    Amino acid composition bias correction bug removed.

--- src/prefiltering/QueryTemplateMatcher.cpp
+++ src/prefiltering/QueryTemplateMatcher.cpp
@@ -113,7 +113,7 @@ void QueryTemplateMatcher::match(Sequence* seq){
                 std::cout << " " << retList[i].first << "\n";
             }*/
             std::pair<short,unsigned int> kmerMatch = retList[i];
-            short kmerMatchScore = kmerMatch.first + (short) biasCorrection;
+            short kmerMatchScore = std::max((kmerMatch.first + (short) biasCorrection), 0);
             seqList = indexTable->getDBSeqList(kmerMatch.second, &indexTabListSize);
             numMatches += indexTabListSize;
 
