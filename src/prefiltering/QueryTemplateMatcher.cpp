9863ec464b6e5d6a5e3abfa04e6a477d3f0a5627
Maria Hauser <mhauser@genzentrum.lmu.de>
2014-05-13 14:16:01 +0200

    - more efficient check for the underflow during amino acid compositional   bias correction

--- src/prefiltering/QueryTemplateMatcher.cpp
+++ src/prefiltering/QueryTemplateMatcher.cpp
@@ -74,7 +74,6 @@ std::pair<hit_t *, size_t> QueryTemplateMatcher::matchQuery (Sequence * seq, uns
 
 void QueryTemplateMatcher::match(Sequence* seq){
 
-//    Indexer* indexer = new Indexer(m->alphabetSize, kmerSize);
     seq->resetCurrPos();
     
     if (this->aaBiasCorrection)
@@ -90,54 +89,24 @@ void QueryTemplateMatcher::match(Sequence* seq){
     for (int i = 0; i < kmerSize && i < seq->L; i++)
         biasCorrection += deltaS[i];
 
-/*    int overall_score = 0;
-    int match_num = 0;
-    int match_pos = 0;*/
-
     int pos = 0;
-//    std::cout << "\nQUERY: " << seq->getDbKey() << "\n";
+    short zero = 0;
     while(seq->hasNextKmer(kmerSize)){
         const int* kmer = seq->nextKmer(kmerSize);
         // generate k-mer list
         KmerGeneratorResult kmerList = kmerGenerator->generateKmerList(kmer);
         kmerListLen += kmerList.count;
-//        std::cout << pos << ": similar k-mer list length = " << kmerList.count << "\n";
         std::pair<short,unsigned int> * retList = kmerList.scoreKmerList;
 
         // match the index table
-//        int pos_matched = 0;
         for (unsigned int i = 0; i < kmerList.count; i++){
-/*            if (seq->getId() == 1 && pos == 2 ){
-                std::cout << "\t\t";
-                indexer->printKmer(retList[i].second, kmerSize, m->int2aa);
-                std::cout << " " << retList[i].first << "\n";
-            }*/
             std::pair<short,unsigned int> kmerMatch = retList[i];
+            short kmerMatchScore = kmerMatch.first + (short) biasCorrection;
             // avoid unsigned short overflow
-            short kmerMatchScore;
-            if (((int)kmerMatch.first + (int) biasCorrection) < 0 )
-                kmerMatchScore = 0;
-            else
-                kmerMatchScore = kmerMatch.first + (short) biasCorrection;
+            kmerMatchScore = std::max(kmerMatchScore, zero);
 
             seqList = indexTable->getDBSeqList(kmerMatch.second, &indexTabListSize);
             numMatches += indexTabListSize;
-
-            // print k-mer matches query k-mer -> db k-mer with score
-/*            for (int j = 0; j < indexTabListSize; j++){
-                if (seqList[j] == 1){
-                    std::cout << "Similar k-mer list pos: " << i << ", score: " << retList[i].first << ", kmer idx: " << retList[i].second << "\n";
-                    pos_matched = 1;
-                    std::cout << pos << " ";
-                    indexer->printKmer(kmer, kmerSize, m->int2aa);
-                    std::cout << "->";
-                    indexer->printKmer(kmerMatch.second, kmerSize, m->int2aa);
-                    std::cout << "\n";
-                    std::cout << "\t" << kmerMatchScore << "\n";
-                    overall_score+=kmerMatchScore;
-                    match_num++;
-                }
-            }*/
 
             // add the scores for the k-mer to the overall score for this query sequence
             // for the overall score, bit/2 is a sufficient sensitivity and we can use the capacity of unsigned short max score in QueryScore better
@@ -145,16 +114,10 @@ void QueryTemplateMatcher::match(Sequence* seq){
         }
         biasCorrection -= deltaS[pos];
         biasCorrection += deltaS[pos + kmerSize];
-//        if(pos_matched == 1)
-//            match_pos++;
         pos++;
     }
-/*    std::cout << "score = " << overall_score << "\n";
-    std::cout << "matched at " << match_pos << " positions.\n";
-    std::cout << "matched " << match_num << " times.\n";*/
     // write statistics
     seq->stats->kmersPerPos = ((float)kmerListLen/(float)seq->L);
     seq->stats->dbMatches = numMatches;
 
-//    delete indexer;
 }
