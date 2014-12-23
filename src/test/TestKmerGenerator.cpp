4be228abf3747f69c73d17e9dfe90b2dad7629d4
Martin Steinegger <mad@Martins-MBP-3.fritz.box>
2014-12-24 00:50:23 +0100

    add kmer test to cmake

--- src/test/TestKmerGenerator.cpp
+++ src/test/TestKmerGenerator.cpp
@@ -19,7 +19,7 @@ int main (int argc, const char * argv[])
     const size_t kmer_size=6;
 
 
-    SubstitutionMatrix subMat("../../data/blosum62.out",8.0);
+    SubstitutionMatrix subMat("/Users/mad/Documents/workspace/mmseqs/data/blosum62.out",8.0);
     std::cout << "Subustitution matrix:\n";
 
     //   BaseMatrix::print(subMat.subMatrix, subMat.alphabetSize);
@@ -28,7 +28,8 @@ int main (int argc, const char * argv[])
     std::cout << "subMatrix:\n";
     //    ReducedMatrix subMat(subMat.probMatrix, 20);
     //   BaseMatrix::print(subMat.subMatrix, subMat.alphabetSize);
-    std::cout << "\n";
+    std::cout <<  std::endl;
+    std::cout << "ExtSupMatrix:"<< std::endl;
 
     const int  testSeq[]={1,2,3,1,1,1};
     ExtendedSubstitutionMatrix extMattwo(subMat.subMatrix, 2,subMat.alphabetSize);
@@ -36,12 +37,12 @@ int main (int argc, const char * argv[])
 
     Indexer idx(subMat.alphabetSize,kmer_size);
     std::cout << "Sequence (id 0):\n";
-    const char* sequence = argv[1];
+    const char* sequence = "PATWPCLVALG";
     std::cout << sequence << "\n\n";
-    Sequence* s = new Sequence (10000, subMat.aa2int, subMat.int2aa, 0, kmer_size, true);
+    Sequence* s = new Sequence (10000, subMat.aa2int, subMat.int2aa, 0, kmer_size, false);
     s->mapSequence(0,"lala",sequence);
 
-    KmerGenerator kmerGen(kmer_size,subMat.alphabetSize,114);
+    KmerGenerator kmerGen(kmer_size,subMat.alphabetSize,161);
 
     kmerGen.setDivideStrategy(extMatthree.scoreMatrix, extMattwo.scoreMatrix );
     int* testKmer = new int[kmer_size];
@@ -51,7 +52,9 @@ int main (int argc, const char * argv[])
         printf("Pos1: %d\n",i++);
 
         unsigned int idx_val=idx.int2index(curr_pos);
-        std::cout << "Index:    " <<idx_val << "\n";
+        std::cout << "Index:    " <<idx_val << "  ";
+        idx.printKmer(idx_val, kmer_size, subMat.int2aa);
+        std::cout << std::endl;
 //        std::cout << "MaxScore: " << extMattwo.scoreMatrix[idx_val]->back().first<< "\n";
         ScoreMatrix kmer_list= kmerGen.generateKmerList(curr_pos);
         std::cout << "Similar k-mer list size:" << kmer_list.elementSize << "\n\n";
