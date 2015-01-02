78f8114ae7fe58893ae73779ddf0cb2b34a9777a
Milot Mirdita <milot@mirdita.de>
2015-01-02 21:36:37 +0100

    Updated cstranslate parameters

--- util/cstranslate_wrapper.sh
+++ util/cstranslate_wrapper.sh
@@ -6,7 +6,7 @@ trap "rm -f $tmp; exit 1" 0 1 2 3 13 15
 
 cat /dev/stdin > $tmp
 # parameters x and c were optimized for uniprot a long time ago
-cstranslate -x 0.3 -c 4 -i $tmp -o stdout -A $HHLIB/data/cs219.lib 2>/dev/null
+cstranslate -b -x 0.3 -c 4 -i $tmp -o stdout -A $HHLIB/data/cs219.lib -D $HHLIB/data/context_data.lib -I a3m 2>/dev/null
 
 rm -f $tmp
 trap 0
