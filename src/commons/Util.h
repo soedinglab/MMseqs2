4fd998fc988fc5dca7465930432736b8fb822f85
Milot Mirdita <milot@mirdita.de>
2015-03-19 15:25:45 +0100

    moved util function into the .c file to not polute the includes

--- src/commons/Util.h
+++ src/commons/Util.h
@@ -8,7 +8,6 @@
 #include <limits.h>
 
 #include <iostream>
-#include <sys/stat.h>
 
 extern "C" {
 #include "ffindex.h"
@@ -287,14 +286,7 @@ public:
         key[keySize] = '\0';
     }
 
-    static FILE* openFileOrDie(const char * fileName, const char * mode) {
-        struct stat st;
-        FILE* file;
-        if(stat(fileName, &st) == 0) { errno = EEXIST; perror(fileName); exit(EXIT_FAILURE); }
-        file = fopen(fileName, mode);
-        if(file == NULL) { perror(fileName); exit(EXIT_FAILURE); }
-        return file;
-	}
+    static FILE* openFileOrDie(const char * fileName, const char * mode);
 
 	static inline void parseByColumnNumber(char *data, char * key, int position) {
         char * startPosOfKey = data;
