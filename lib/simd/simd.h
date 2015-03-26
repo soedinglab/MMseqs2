fad528633c8d2d9480efc142da5a90d2ced49836
mad <mad@Martins-MacBook-Pro.local>
2015-03-27 00:42:21 +0100

    Create methode to compute PSSM

--- lib/simd/simd.h
+++ lib/simd/simd.h
@@ -451,4 +451,69 @@ inline simd_int * malloc_simd_int(const size_t size)
     return (simd_int *) mem_align(ALIGN_INT,size);
 }
 #endif
+
+inline float ScalarProd20(const float* qi, const float* tj) {
+
+//#ifdef AVX
+//  float __attribute__((aligned(ALIGN_FLOAT))) res;
+//  __m256 P; // query 128bit SSE2 register holding 4 floats
+//  __m256 S; // aux register
+//  __m256 R; // result
+//  __m256* Qi = (__m256*) qi;
+//  __m256* Tj = (__m256*) tj;
+//
+//  R = _mm256_mul_ps(*(Qi++),*(Tj++));
+//  P = _mm256_mul_ps(*(Qi++),*(Tj++));
+//  S = _mm256_mul_ps(*Qi,*Tj); // floats A, B, C, D, ?, ?, ? ,?
+//  R = _mm256_add_ps(R,P);     // floats 0, 1, 2, 3, 4, 5, 6, 7
+//  P = _mm256_permute2f128_ps(R, R, 0x01); // swap hi and lo 128 bits: 4, 5, 6, 7, 0, 1, 2, 3
+//  R = _mm256_add_ps(R,P);     // 0+4, 1+5, 2+6, 3+7, 0+4, 1+5, 2+6, 3+7
+//  R = _mm256_add_ps(R,S);     // 0+4+A, 1+5+B, 2+6+C, 3+7+D, ?, ?, ? ,?
+//  R = _mm256_hadd_ps(R,R);    // 04A15B, 26C37D, ?, ?, 04A15B, 26C37D, ?, ?
+//  R = _mm256_hadd_ps(R,R);    // 01234567ABCD, ?, 01234567ABCD, ?, 01234567ABCD, ?, 01234567ABCD, ?
+//  _mm256_store_ps(&res, R);
+//  return res;
+//#else
+//
+//
+//TODO fix this
+//#ifdef SSE
+//    float __attribute__((aligned(16))) res;
+//    __m128 P; // query 128bit SSE2 register holding 4 floats
+//    __m128 R;// result
+//    __m128* Qi = (__m128*) qi;
+//    __m128* Tj = (__m128*) tj;
+//
+//    __m128 P1 = _mm_mul_ps(*(Qi),*(Tj));
+//    __m128 P2 = _mm_mul_ps(*(Qi+1),*(Tj+1));
+//    __m128 R1 = _mm_add_ps(P1, P2);
+//
+//    __m128 P3 = _mm_mul_ps(*(Qi + 2), *(Tj + 2));
+//    __m128 P4 = _mm_mul_ps(*(Qi + 3), *(Tj + 3));
+//    __m128 R2 = _mm_add_ps(P3, P4);
+//    __m128 P5 = _mm_mul_ps(*(Qi+4), *(Tj+4));
+//
+//    R = _mm_add_ps(R1, R2);
+//    R = _mm_add_ps(R,P5);
+//
+////    R = _mm_hadd_ps(R,R);
+////    R = _mm_hadd_ps(R,R);
+//    P = _mm_shuffle_ps(R, R, _MM_SHUFFLE(2,0,2,0));
+//    R = _mm_shuffle_ps(R, R, _MM_SHUFFLE(3,1,3,1));
+//    R = _mm_add_ps(R,P);
+//    P = _mm_shuffle_ps(R, R, _MM_SHUFFLE(2,0,2,0));
+//    R = _mm_shuffle_ps(R, R, _MM_SHUFFLE(3,1,3,1));
+//    R = _mm_add_ps(R,P);
+//    _mm_store_ss(&res, R);
+//    return res;
+//#endif
+//#endif
+    return tj[0] * qi[0] + tj[1] * qi[1] + tj[2] * qi[2] + tj[3] * qi[3]
+            + tj[4] * qi[4] + tj[5] * qi[5] + tj[6] * qi[6] + tj[7] * qi[7]
+            + tj[8] * qi[8] + tj[9] * qi[9] + tj[10] * qi[10] + tj[11] * qi[11]
+            + tj[12] * qi[12] + tj[13] * qi[13] + tj[14] * qi[14]
+            + tj[15] * qi[15] + tj[16] * qi[16] + tj[17] * qi[17]
+            + tj[18] * qi[18] + tj[19] * qi[19];
+}
+
 #endif //SIMD_H
--- src/alignment/PSSM.h
+++ src/alignment/PSSM.h
@@ -6,7 +6,55 @@
 #define _MMSEQS_PSSM_H_
 
 
+#include <SubstitutionMatrix.h>
+#include <stddef.h>
+#include "MultipleAlignment.h"
+
 class PSSM {
+public:
+
+    PSSM(SubstitutionMatrix *subMat, size_t maxSeqLength);
+
+    ~PSSM();
+
+    void computePSSMFromMSA(MultipleAlignment::MSAResult msaResult);
+    void print(size_t queryLength);
+private:
+    SubstitutionMatrix * subMat;
+    size_t maxSeqLength;
+    unsigned char * pssm;
+    float * profile;
+    bool * gapPosition;
+    // compute position frequency matrix
+    // M_{aa,pos}=\frac{1}{N} \sum_{i=1}^N I(X_{i,pos}=aa)
+    void computeFrequencyMatrix(size_t setSize, char const **msaSeqs, size_t msaSeqLength);
+
+    // compute position-specific scoring matrix PSSM score
+    // 1.) convert PFM to PPM (position probability matrix)
+    //     Both PPMs assume statistical independence between positions in the pattern
+    // 2.) PSSM Log odds score
+    //     M_{aa,pos}={log(M_{aa,pos} / b_{aa}).
+    void computeLogPSSM(float *profile, size_t queryLength);
+
+    float * seqWeight;
+    float * frequency;
+    float * frequency_with_pseudocounts;
+    float * Neff_M;
+
+    // normalize a fector to 1.0
+    float NormalizeTo1(float *array, int length, double const *def_array);
+
+
+    void PreparePseudocounts(float *frequency, float *frequency_with_pseudocounts, size_t queryLength, const float **R);
+
+    // compute the Neff_M per column -p log(p)
+    void computeNeff_M(float *frequency, float *seqWeight, float *Neff_M, size_t queryLength, size_t setSize, char const **msaSeqs);
+
+    // Compute weight for sequence based on "Position-based Sequence Weights' (1994)
+    void computeSequenceWeights(float *seqWeight, size_t queryLength, size_t setSize, const char **msaSeqs);
+
+    // compute pseudocounts from Neff_M -p log(p) per column
+    void computePseudoCounts(float *profile, float *frequency, float *frequency_with_pseudocounts, size_t length);
 
 };
 
