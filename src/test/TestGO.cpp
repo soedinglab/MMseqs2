a5710f0abec28f6269290f4b0f28309293d8326c
Lars von den Driesch <larsvdd@gmx.net>
2015-05-07 15:55:55 +0100

    Implementation of GOScoring with generation of evaluation files

--- src/test/TestGO.cpp
+++ src/test/TestGO.cpp
@@ -8,8 +8,12 @@ int main(int argc, char **argv)
 {
 
     CompareGOTerms* go=new CompareGOTerms("/home/lars/masterarbeit/data/GO/go-basic_ffindex","/home/lars/masterarbeit/data/GO/go-basic_ffindex.index",
-            "/home/lars/masterarbeit/data/uniprot/release-2015_04/uniprot_sprot_go_db", "/home/lars/masterarbeit/data/uniprot/release-2015_04/uniprot_sprot_go_db.index");
+            "/home/lars/masterarbeit/data/uniprot/release-2015_04/uniprot_sprot_go_db", "/home/lars/masterarbeit/data/uniprot/release-2015_04/uniprot_sprot_go_db.index","/home/lars/masterarbeit/data/uniprot/release-2015_04/evaluation");
 
     go->init();
+
+    //go->all_against_all_comparison();
+  //  go->all_against_all_comparison_proteinset();
+    go->run_evaluation_mmseqsclustering("/home/lars/masterarbeit/db/sprot/uniprot_sprot_s4_affinity","/home/lars/masterarbeit/db/sprot/uniprot_sprot_s4_affinity.index");
 }
 
