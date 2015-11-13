da15dd8d494264e760ca8524f2b4ead695c036d0
mad <mad@Martins-MacBook-Pro.local>
2015-11-13 15:43:26 +0900

    Missing flag for createindex

--- src/util/ffindex_to_pref_index.cpp
+++ src/util/ffindex_to_pref_index.cpp
@@ -33,7 +33,7 @@ int createindex (int argc, const char * argv[])
 
     Sequence seq(par.maxSeqLen, subMat->aa2int, subMat->int2aa, Sequence::AMINO_ACIDS, par.kmerSize, par.spacedKmer);
 
-    PrefilteringIndexReader::createIndexFile(par.db2, par.db2Index, &dbr, &seq, par.split, subMat->alphabetSize, par.kmerSize, par.skip, par.spacedKmer, par.searchMode);
+    PrefilteringIndexReader::createIndexFile(par.db2, par.db2Index, &dbr, &seq, par.split, subMat->alphabetSize, par.kmerSize, par.skip, par.spacedKmer, par.searchMode, par.mask);
 
     // write code
     dbr.close();
