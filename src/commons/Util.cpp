fe3f20a659f80b6a25bed62654275cfa4f7a0c6b
Milot Mirdita <milot@mirdita.de>
2015-11-18 20:22:46 +0100

    Be explicit if a file should exist, also fixed wrong code

--- src/commons/Util.cpp
+++ src/commons/Util.cpp
@@ -37,7 +37,7 @@ void Util::decompose_domain(size_t domain_size, size_t world_rank,
 
 std::map<std::string, size_t> Util::readMapping(const char *fastaFile) {
     std::map<std::string, size_t> map;
-    FILE * fasta_file = Util::openFileOrDie(fastaFile, "r");
+    FILE * fasta_file = Util::openFileOrDie(fastaFile, "r", true);
     kseq_t *seq = kseq_init(fileno(fasta_file));
     size_t i = 0;
     while (kseq_read(seq) >= 0) {
@@ -147,11 +147,25 @@ std::string Util::parseFastaHeader(std::string header){
     return arr[0];
 }
 
-
-FILE* Util::openFileOrDie(const char * fileName, const char * mode) {
+bool Util::fileExists(const char* fileName) {
     struct stat st;
+    return stat(fileName, &st) == 0;
+}
+
+FILE* Util::openFileOrDie(const char * fileName, const char * mode, bool shouldExist) {
+    bool exists = Util::fileExists(fileName);
+    if(exists && !shouldExist) {
+        errno = EEXIST;
+        perror(fileName);
+        EXIT(EXIT_FAILURE);
+    }
+    if(!exists && shouldExist) {
+        errno = ENOENT;
+        perror(fileName);
+        EXIT(EXIT_FAILURE);
+    }
+
     FILE* file;
-    if(stat(fileName, &st) == 0) { errno = EEXIST; perror(fileName); EXIT(EXIT_FAILURE); }
     file = fopen(fileName, mode);
     if(file == NULL) { perror(fileName); EXIT(EXIT_FAILURE); }
     return file;
--- src/commons/Util.h
+++ src/commons/Util.h
@@ -229,7 +229,9 @@ public:
         key[keySize] = '\0';
     }
 
-    static FILE* openFileOrDie(const char * fileName, const char * mode);
+    static bool fileExists(const char* fileName);
+
+    static FILE* openFileOrDie(const char * fileName, const char * mode, bool shouldExist);
 
 	static inline void parseByColumnNumber(char *data, char * key, int position) {
         char * startPosOfKey = data;
