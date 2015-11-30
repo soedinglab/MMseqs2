94d263b7ffd71c502f4074474bb24f0785482272
Lars von den Driesch <larsvdd@gmx.net>
2015-11-30 10:53:28 +0100

    seqid filter for testgo adjusted to new alignmentfile format

--- src/util/convertfiles.h
+++ src/util/convertfiles.h
@@ -5,10 +5,13 @@
 #ifndef MMSEQS_CONVERTFILES_H
 #define MMSEQS_CONVERTFILES_H
 #include <Util.h>
+#include <DBReader.h>
 
 class convertfiles{
 
 public:
+
+    convertfiles(std::string sequencedb);
     void getAlignmentscoresForCluster(std::string clusteringfile, std::string alignmentfile, std::string outputfile);
 
     void convertDomainFileToFFindex(std::string domainscorefile, std::string domainIdentifierFile, std::string outputfile);
@@ -16,7 +19,12 @@ public:
     void getDomainScoresForCluster(std::string clusteringfile, std::string alignmentfile, std::string outputfolder, std::string prefix, bool allagainstall,bool randomized);
 
     void convertFfindexToTsv(std::string clusteringfile, std::string prefix, std::string outputfolder);
-};
+
+private:
+    DBReader* targetdb_header;
+    std::string getProteinNameForID(const char * dbKey);
+
+    };
 
 
 #endif //MMSEQS_CONVERTFILES_H
