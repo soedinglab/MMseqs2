5bb3a0aea77865d1aabc9ccc83cff7d270653e30
Martin Steinegger <themartinsteinegger@gmail.com>
2015-12-17 16:04:48 +0100

    Add Diagonal Matcher

---
+++ src/prefiltering/DiagonalMatcher.h
@@ -0,0 +1,14 @@
+//
+// Created by mad on 12/15/15.
+//
+
+#ifndef MMSEQS_DIAGONALMATCHER_H
+#define MMSEQS_DIAGONALMATCHER_H
+
+
+class DiagonalMatcher {
+
+};
+
+
+#endif //MMSEQS_DIAGONALMATCHER_H
