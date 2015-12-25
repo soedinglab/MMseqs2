b53526b84848508c61dc79cc7f22a0bb4cf8eb9a
Milot Mirdita <milot@mirdita.de>
2015-12-25 04:18:58 +0100

    allow the bash completion to suggest files too

--- util/bash-completion.sh
+++ util/bash-completion.sh
@@ -4,7 +4,6 @@ _mmseqs() {
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts=""
 
 	if [[ ${COMP_CWORD} -eq 1 ]] ; then
 		COMPREPLY=( $(compgen -W "$(mmseqs shellcompletion)" -- ${cur}) )
@@ -12,7 +11,7 @@ _mmseqs() {
 	fi
 
 	if [[ ${COMP_CWORD} -gt 1 ]] ; then
-		COMPREPLY=( $(compgen -W "$(mmseqs shellcompletion ${COMP_WORDS[1]})" -- ${cur}) )
+		COMPREPLY=( $(compgen -f -W "$(mmseqs shellcompletion ${COMP_WORDS[1]})" -- ${cur}) )
 		return 0
 	fi
 
