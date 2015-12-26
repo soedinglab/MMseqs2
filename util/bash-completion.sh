be836b35d8737f2dd89e9b0c34c26d1b6619bd2f
Milot Mirdita <milot@mirdita.de>
2015-12-26 15:46:41 +0100

    cleanup

--- util/bash-completion.sh
+++ util/bash-completion.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 _mmseqs() {
-	local cur prev opts
+	local cur
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
 
 	if [[ ${COMP_CWORD} -eq 1 ]] ; then
-		COMPREPLY=( $(compgen -W "$(mmseqs shellcompletion)" -- ${cur}) )
+		COMPREPLY=( $(compgen -W "$(mmseqs shellcompletion)" -- "${cur}") )
 		return 0
 	fi
 
 	if [[ ${COMP_CWORD} -gt 1 ]] ; then
-		COMPREPLY=( $(compgen -f -W "$(mmseqs shellcompletion ${COMP_WORDS[1]})" -- ${cur}) )
+		COMPREPLY=( $(compgen -f -W "$(mmseqs shellcompletion "${COMP_WORDS[1]}")" -- "${cur}") )
 		return 0
 	fi
 
