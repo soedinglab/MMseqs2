1eb91205c8bd5ed267c0a1c83c561d3af9f4b709
Milot Mirdita <milot@mirdita.de>
2016-01-20 17:21:09 +0100

    Removed useless findsorf tool and moved extractorf to util

--- src/commons/CommandDeclarations.h
+++ src/commons/CommandDeclarations.h
@@ -19,7 +19,6 @@ extern int createprofiledb(int argc, const char **argv);
 extern int translatenucleotide(int argc, const char **argv);
 extern int timetest(int argc, const char **argv);
 extern int legacycs219(int argc, const char **argv);
-extern int findsorf(int argc, const char **argv);
 extern int result2profile(int argc, const char **argv);
 extern int splitffindex(int argc, const char **argv);
 extern int formatalignment(int argc, const char **argv);
