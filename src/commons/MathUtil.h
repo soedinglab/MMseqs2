601b6ad69ec24a9f8ddf366ad04b66827a01bb07
Milot Mirdita <milot@mirdita.de>
2016-01-28 12:53:18 +0100

    Move _log2 from BaseMatrix to MathUtil

--- src/commons/MathUtil.h
+++ src/commons/MathUtil.h
@@ -45,6 +45,7 @@ public:
         return res;
     }
 
+    // doesnt produce NANs careful
     static inline float flog2(float x) {
         if (x <= 0)
             return -128;
@@ -91,6 +92,8 @@ public:
             pow *= 10;
         return x * pow + y;
     }
+
+    static inline double log2(double x) { return log10(x)/0.301029996; }
 };
 
 #endif
