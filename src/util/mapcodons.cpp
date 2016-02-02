44b9e5641c1ec0bb18d661e322714760986e21a1
Milot Mirdita <milot@mirdita.de>
2016-02-02 14:26:45 +0100

    Add wip mapcodons

--- src/util/mapcodons.cpp
+++ src/util/mapcodons.cpp
@@ -5,27 +5,49 @@
 #include "Log.h"
 #include "Debug.h"
 
+extern "C" {
+#include "ext/fmemopen.h"
+}
+
+#include <unistd.h>
+#include "kseq.h"
+KSEQ_INIT(int, read)
 
 #ifdef OPENMP
 #include <omp.h>
 #endif
 
-int mapcodons(std::string inputDb, std::string outputDb, int threads, int column, std::string regexStr) {
-    DBReader<std::string> reader(DBReader<std::string>(inputDb.c_str(), (std::string(inputDb).append(".index")).c_str()));
+
+int mapcodons(int argn, const char **argv)
+{
+    std::string usage;
+    usage.append("Maps back the original nucleotides to a MSA produced by MMseqs\n");
+    usage.append("USAGE: <msaDB> <ncDB> <outDB>\n");
+    usage.append("\nDesigned and implemented by Milot Mirdita <milot@mirdita.de>.\n");
+
+    Parameters par;
+    par.parseParameters(argn, argv, usage, par.onlyverbosity, 4);
+#ifdef OPENMP
+    omp_set_num_threads(par.threads);
+#endif
+
+    DBReader<std::string> reader(DBReader<std::string>(par.db1.c_str(), par.db1Index.c_str()));
     reader.open(DBReader<std::string>::LINEAR_ACCCESS);
 
-    DBWriter writer(outputDb.c_str(), (std::string(outputDb).append(".index")).c_str(), threads);
-    writer.open();
+    DBReader<std::string> ncReader(DBReader<std::string>(par.db3.c_str(), par.db3Index.c_str()));
+    ncReader.open(DBReader<std::string>::NOSORT);
+
+    //DBWriter writer(par.db4.c_str(), par.db4Index.c_str(), par.threads);
+    //writer.open();
 
     const size_t LINE_BUFFER_SIZE = 1000000;
 
 #pragma omp parallel
     {
-        char *lineBuffer = new char[LINE_BUFFER_SIZE];
-        char *columnValue = new char[LINE_BUFFER_SIZE];
-        char **columnPointer = new char*[column + 1];
         std::string buffer = "";
         buffer.reserve(LINE_BUFFER_SIZE);
+
+
 #pragma omp for schedule(dynamic, 100)
         for (size_t id = 0; id < reader.getSize(); id++) {
             Log::printProgress(id);
@@ -33,43 +55,26 @@ int mapcodons(std::string inputDb, std::string outputDb, int threads, int column
 #ifdef OPENMP
             thread_idx = omp_get_thread_num();
 #endif
+
             char *data = reader.getData(id);
+            size_t dataLength = reader.getSeqLens(id);
 
-            while (*data != '\0') {
-                //Util::getLine(data, lineBuffer);
-                size_t foundElements = Util::getWordsOfLine(lineBuffer, columnPointer, column + 1);
-
-                data = Util::skipLine(data);
+            FILE* file = fmemopen(data, dataLength, "r");
+            kseq_t *seq = kseq_init(fileno(file));
+            while (kseq_read(seq) >= 0) {
+                Debug(Debug::INFO) << seq->comment.s << "\n";
+                //char * ncSeq = ncReader.getData(id);
+                //size_t ncSeqLength = ncReader.getSeqLens(id);
             }
+            kseq_destroy(seq);
+            fclose(file);
 
-            writer.write(buffer.c_str(), buffer.length(), (char*) reader.getDbKey(id).c_str(), thread_idx);
-            buffer.clear();
+            //writer.write(buffer.c_str(), buffer.length(), (char*) reader.getDbKey(id).c_str(), thread_idx);
         }
-        delete [] lineBuffer;
-        delete [] columnValue;
-        delete [] columnPointer;
     }
 
+    ncReader.close();
     reader.close();
-    writer.close();
-    return 0;
-}
-
-int filterdb(int argn, const char **argv)
-{
-    std::string usage;
-    usage.append("Filter a database by column regex\n");
-    usage.append("USAGE:  <ffindexDB> <outDB>\n");
-    usage.append("\nDesigned and implemented by Martin Steinegger <martin.steinegger@mpibpc.mpg.de>.\n");
-
-    Parameters par;
-    par.parseParameters(argn, argv, usage, par.filterDb, 2);
-#ifdef OPENMP
-    omp_set_num_threads(par.threads);
-#endif
-    return mapcodons(par.db1,
-                    par.db2,
-                    par.threads,
-                    par.filterColumn,
-                    par.filterColumnRegex);
+    //writer.close();
+    return EXIT_SUCCESS;
 }
