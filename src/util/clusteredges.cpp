29213ac5b1ff97b5c88920c5a78c82b0ddcd253a
Milot Mirdita <milot@mirdita.de>
2016-02-11 14:43:17 +0100

    Clusterdges works now

--- src/util/clusteredges.cpp
+++ src/util/clusteredges.cpp
@@ -4,12 +4,9 @@
 #include "Util.h"
 #include "FileUtil.h"
 
-#include <climits>
-#include <sstream>
 #include <fstream>
 
-int clusteredges(int argc, const char * argv[])
-{
+int clusteredges(int argc, const char *argv[]) {
     std::string usage("Extracts all child to parent edges from clustering N to clustering N+1.\n");
     usage.append("Written by Milot Mirdita <milot@mirdita.de>.\n\n");
     usage.append("USAGE: <edgeListOut> <clusteringSteps...> \n");
@@ -20,20 +17,18 @@ int clusteredges(int argc, const char * argv[])
 
     size_t clusterings = static_cast<size_t >(argc - 1);
 
-    if(clusterings < 2) {
-        Debug(Debug::ERROR) << "Need at least two cluster results to extract edges\n";
+    if (clusterings == 0) {
+        Debug(Debug::ERROR) << "Need at least one cluster result to extract edges\n";
         EXIT(EXIT_FAILURE);
     }
 
-    typedef DBReader<unsigned int> Reader;
-
-    Reader* readers[clusterings];
-    for(int i = 0; i < clusterings; ++i) {
-        std::pair<std::string, std::string> name = Util::databaseNames(argv[i+1]);
-        readers[i] = new Reader(name.first.c_str(), name.second.c_str());
+    DBReader<unsigned int> *readers[clusterings];
+    for (int i = 0; i < clusterings; ++i) {
+        std::pair<std::string, std::string> name = Util::databaseNames(argv[i + 1]);
+        readers[i] = new DBReader<unsigned int>(name.first.c_str(), name.second.c_str());
     }
 
-    if(FileUtil::fileExists(par.db1.c_str())) {
+    if (FileUtil::fileExists(par.db1.c_str())) {
         errno = EEXIST;
         perror(par.db1.c_str());
         EXIT(EXIT_FAILURE);
@@ -42,45 +37,41 @@ int clusteredges(int argc, const char * argv[])
     std::ofstream edgeList;
     edgeList.open(par.db1);
 
-    if(edgeList.fail()) {
+    if (edgeList.fail()) {
         Debug(Debug::ERROR) << "Could not open " << par.db1 << " for writing.";
         EXIT(EXIT_FAILURE);
     }
 
     Debug(Debug::INFO) << "Start writing file to " << par.db1 << "\n";
-    for(size_t i = 1; i < clusterings; ++i){
-        Reader* parentReader = readers[i - 1];
-        parentReader->open(Reader::NOSORT);
-        Reader* childReader  = readers[i];
-        childReader->open(Reader::NOSORT);
+    for (size_t i = 0; i < clusterings; ++i) {
+        DBReader<unsigned int> *reader = readers[i];
+        reader->open(DBReader<unsigned int>::NOSORT);
 
-        for(size_t j = 0; j < parentReader->getSize(); ++j) {
-            unsigned int parentKey = parentReader->getDbKey(j);
+        for (size_t j = 0; j < reader->getSize(); ++j) {
+            unsigned int parentKey = reader->getDbKey(j);
 
-            if(i == 1) {
-                // add the implicit root explicitly
+            // add the implicit root explicitly
+            if (i == 0) {
                 edgeList << parentKey << "\tNULL\n";
             }
 
-            std::istringstream ss(parentReader->getData(j));
+            std::istringstream ss(reader->getData(j));
 
             std::string line;
             std::getline(ss, line);
-            while(std::getline(ss, line)) {
-                if(line.length() < 1)
+            while (std::getline(ss, line)) {
+                if (line.length() < 1)
                     continue;
                 unsigned int childKey = static_cast<unsigned int>(strtoul(line.c_str(), NULL, 10));
-                //char* childData = childReader.getDataByDBKey(childKey);
 
                 edgeList << childKey << "\t" << parentKey << "\n";
             }
         }
 
-        childReader->close();
-        parentReader->close();
+        reader->close();
     }
 
-    for(int i = 0; i < clusterings; ++i) {
+    for (int i = 0; i < clusterings; ++i) {
         delete readers[i];
     }
 
