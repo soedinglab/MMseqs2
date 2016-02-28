56b5227bf73138e49d047472aa2b64679c410077
Milot Mirdita <milot@mirdita.de>
2016-02-28 15:27:14 +0100

    Simple Homebrew formula for MMseqs

---
+++ Formula/mmseqs.rb
@@ -0,0 +1,33 @@
+class Mmseqs < Formula
+  desc "MMseqs (Many-against-Many sequence searching) is a software suite for very fast protein sequence searches and clustering of huge protein sequence data sets."
+  homepage "https://github.com/soedinglab/mmseqs"
+  head "git@bitbucket.org:soedinglab/mmseqs-dev.git", :using => :git
+
+  option "with-tests", "Build MMseqs test utils"
+
+  depends_on "open-mpi" => :optional
+  depends_on "cmake" => :build
+
+  def install
+    args = *std_cmake_args
+
+    args << (build.with?("open-mpi") ? "-DHAVE_MPI=1" : "-DHAVE_MPI=0")
+    args << (build.with?("tests") ? "-DHAVE_TESTS=1" : "-DHAVE_TESTS=0")
+
+    system "cmake", ".", *args
+    system "make"
+
+    bin.install "src/mmseqs"
+    bin.install "src/workflow/blastp.sh"
+    bin.install "src/workflow/blastpgp.sh"
+    bin.install "src/workflow/clustering.sh"
+    bin.install "src/workflow/cascaded_clustering.sh"
+    prefix.install Dir["data"]
+    doc.install "userguide.pdf"
+    bash_completion.install "util/bash-completion.sh" => "mmseqs.sh"
+  end
+
+  test do
+    system "mmseqs"
+  end
+end
