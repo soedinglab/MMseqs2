d79406f7abbd88310da5e4ca13530e744f3c06c1
mad <mad@Martins-MacBook-Pro.local>
2016-03-07 16:29:54 +0100

    Create shipable mmseqs.tar.gz

--- util/docker/build_image.sh
+++ util/docker/build_image.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -e
-docker build -f Dockerfile.build -t mmseqs-build-image .
+#!/bin/bash -x
 docker run -t mmseqs-build-image:latest
-docker cp $(docker ps -l -q):/root/mmseqs .
+docker cp $(docker ps -l -q):/root/mmseqs/ .
 docker build -f Dockerfile.release -t mmseqs .
+CURR_BUILD=$(pwd)/mmseqs-$(date +%Y-%m-%d-%T)
+mkdir -p $CURR_BUILD
+cp -r mmseqs/bin/ $CURR_BUILD
+cp -r mmseqs/data/ $CURR_BUILD
+cp -r mmseqs/userguide.pdf $CURR_BUILD
+tar cvfz $(pwd)/mmseqs-static.tar.gz -C $CURR_BUILD bin data userguide.pdf
+mv mmdir $CURR_BUILD
 docker run mmseqs 
-rm -rf mmseqs
