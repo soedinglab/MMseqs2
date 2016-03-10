ab277b66846de76b55b731fa6ebc8bb0fbc89b0b
Martin Steinegger <themartinsteinegger@gmail.com>
2016-03-10 01:38:59 +0100

    - merge two read in methods - add more checks to ensure we do not write in wrong memory

--- src/clustering/AlignmentSymmetry.h
+++ src/clustering/AlignmentSymmetry.h
@@ -6,21 +6,28 @@
 #define MMSEQS_ALIGNMENTSYMMETRY_H
 #include <set>
 #include <list>
+#include <Debug.h>
+#include <Util.h>
 
 #include "DBReader.h"
 
 class AlignmentSymmetry {
 public:
-    static void readInData(DBReader<unsigned int>*pReader, DBReader<unsigned int>*pDBReader, unsigned int **pInt);
-    static void readInData(DBReader<unsigned int>*pReader, DBReader<unsigned int>*pDBReader, unsigned int **pInt,unsigned short**elementScoreTable, int scoretype);
-    static void computeOffsetTable(size_t *elementSizes, size_t dbSize);
+    static void readInData(DBReader<unsigned int>*pReader, DBReader<unsigned int>*pDBReader, unsigned int **pInt,unsigned short**elementScoreTable, int scoretype, size_t *offsets);
+    static void computeOffsetFromCounts(size_t *elementSizes, size_t dbSize);
     static size_t findMissingLinks(unsigned int **elementLookupTable, size_t *offsetTable, size_t dbSize, int threads);
     static void addMissingLinks(unsigned int **elementLookupTable, size_t *offsetTable, size_t dbSize,unsigned short**elementScoreTable);
     static void sortElements(unsigned int **elementLookupTable, size_t *offsets, size_t dbSize);
 
     template <typename T>
-    static void setupLookupPointer(T *elements, T **elementLookupTable, size_t *elementOffset, size_t dbSize) {
+    static void setupPointers(T *elements, T **elementLookupTable, size_t *elementOffset,
+                                 unsigned int dbSize, size_t totalElementCount) {
         for(size_t i = 0; i < dbSize; i++) {
+            if(totalElementCount < elementOffset[i]){
+                Debug(Debug::ERROR) << "Error in setupPointers. totalElementCount "
+                                    << "(" << totalElementCount << ") < elementOffset[i] (" << elementOffset[i] << ")\n";
+                EXIT(EXIT_FAILURE);
+            }
             elementLookupTable[i] = elements + elementOffset[i];
         }
     }
