dd42b4107898799d82e10a1932c1c4309a072998
Martin Steinegger <themartinsteinegger@gmail.com>
2016-03-13 19:48:39 +0100

    Add util to static build

--- util/docker/build_image.sh
+++ util/docker/build_image.sh
@@ -7,7 +7,9 @@ CURR_BUILD=$(pwd)/mmseqs-$(date +%Y-%m-%d-%T)
 mkdir -p $CURR_BUILD
 cp -r mmseqs/mmseqs/bin $CURR_BUILD
 cp -r mmseqs/data $CURR_BUILD
+cp -r mmseqs/util $CURR_BUILD
+rm -rf $CURR_BUILD/util/docker
 cp -r mmseqs/userguide.pdf $CURR_BUILD
-tar cvfz $(pwd)/mmseqs-static.tar.gz -C $CURR_BUILD bin data userguide.pdf
+tar cvfz $(pwd)/mmseqs-static.tar.gz -C $CURR_BUILD bin data util userguide.pdf
 mv mmseqs $CURR_BUILD
 docker run mmseqs 
