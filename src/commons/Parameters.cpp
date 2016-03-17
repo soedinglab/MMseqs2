c557c9903bd9fb69f81df7c1de88d0aea27e4c0d
Milot Mirdita <milot@mirdita.de>
2016-03-17 17:00:58 +0100

    Initialize runner string with runner env only if it actually has a value

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -601,7 +601,12 @@ void Parameters::setDefaults() {
     similarityScoreType=APC_SEQID;
 
     // workflow
-    runner = getenv("RUNNER");
+    const char *runnerEnv = getenv("RUNNER");
+    if (runnerEnv != NULL) {
+        runner = runnerEnv;
+    } else {
+        runner = "";
+    }
 
     // Clustering workflow
     removeTmpFiles = false;
