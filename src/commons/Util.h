39dc093f1f9a4975a0be4780ca4baf679d530e0a
mad <mad@Martins-MacBook-Pro.local>
2016-05-17 20:28:12 +0900

    Set split size automatically based on the main memory of the machine.

--- src/commons/Util.h
+++ src/commons/Util.h
@@ -46,7 +46,8 @@ public:
     static void rankedDescSort20(short *val, unsigned int *index);
     static void decomposeDomainByAminoAcid(size_t aaSize, unsigned int *seqSizes, size_t count,
                                            size_t worldRank, size_t worldSize, size_t *start, size_t *end);
-
+    static size_t getTotalSystemMemory();
+    static size_t get_phys_pages();
     static size_t countLines(const char *data, size_t length);
 
     static inline int fast_atoi( const char * str )
