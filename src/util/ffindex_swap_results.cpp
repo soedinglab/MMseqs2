43b76a7a86e009049d3bbc9192b083cf3a210a74
Milot Mirdita <milot@mirdita.de>
2016-06-17 17:19:04 +0200

    Another try at at a far simpler MPI swap

--- src/util/ffindex_swap_results.cpp
+++ src/util/ffindex_swap_results.cpp
@@ -3,7 +3,7 @@
 #include "DBReader.h"
 #include "DBWriter.h"
 #include "Debug.h"
-#include "Prefiltering.h"
+#include "Alignment.h"
 #include "Util.h"
 
 #include <fstream>
@@ -12,7 +12,103 @@
 #include <omp.h>
 #endif
 
-#define ENABLE_MPI_SWAP 0
+int doSwap(Parameters &par,
+           const std::vector<std::pair<unsigned int, std::string*>> &swaps,
+           const std::pair<std::string, std::string> &resultdb,
+           size_t queryFrom, size_t querySize) {
+#ifdef OPENMP
+    omp_set_num_threads(par.threads);
+#endif
+
+    double *kmnByLen = NULL;
+    double lambda, logK, lambdaLog2, logKLog2;
+
+    {
+        DBReader<unsigned int> qdbr(par.db1.c_str(), par.db1Index.c_str());
+        qdbr.open(DBReader<unsigned int>::NOSORT);
+        DBReader<unsigned int> tdbr(par.db2.c_str(), par.db2Index.c_str());
+        tdbr.open(DBReader<unsigned int>::NOSORT);
+
+        SubstitutionMatrix subMat(par.scoringMatrixFile.c_str(), 2.0, 0.0);
+        BlastScoreUtils::BlastStat stats = BlastScoreUtils::getAltschulStatsForMatrix(subMat.getMatrixName(),
+                                                                                      Matcher::GAP_OPEN,
+                                                                                      Matcher::GAP_EXTEND);
+
+        int seqLen = static_cast<int>(par.maxSeqLen);
+        kmnByLen = new double[seqLen];
+        for (int len = 0; len < seqLen; len++) {
+            kmnByLen[len] = BlastScoreUtils::computeKmn(len, stats.K, stats.lambda, stats.alpha, stats.beta,
+                                                        qdbr.getAminoAcidDBSize(), qdbr.getSize());
+        }
+
+        lambda = stats.lambda;
+        logK = log(stats.K);
+        lambdaLog2 = lambda / log(2.0);
+        logKLog2 = logK / log(2.0);
+
+        qdbr.close();
+        tdbr.close();
+    }
+
+    // create and open db for writing
+    Debug(Debug::INFO) << "Start to swap results. Write to " << resultdb.first << ".\n";
+    DBWriter splitWriter(resultdb.first.c_str(), resultdb.second.c_str(),
+                         static_cast<unsigned int>(par.threads));
+    splitWriter.open();
+
+    // Write sorted results and delete memory
+#pragma omp parallel for schedule(dynamic,100)
+    for (size_t i = queryFrom; i < queryFrom + querySize; ++i) {
+        unsigned int thread_idx = 0;
+#ifdef OPENMP
+        thread_idx = static_cast<unsigned int>(omp_get_thread_num());
+#endif
+
+        unsigned int id = swaps[i].first;
+        std::string result = *(swaps[i].second);
+        if (result.size() > 1) {
+            char *wordCnt[255];
+            size_t cols = Util::getWordsOfLine((char *) result.c_str(), wordCnt, 254);
+            if (Matcher::ALN_RES_WITH_OUT_BT_COL_CNT >= cols) {
+                std::vector<Matcher::result_t> alnRes = Matcher::readAlignmentResults((char *) result.c_str());
+                for (size_t j = 0; j < alnRes.size(); j++) {
+                    Matcher::result_t &res = alnRes[j];
+                    double rawScore = BlastScoreUtils::bitScoreToRawScore(res.score, lambdaLog2, logKLog2);
+                    res.eval = BlastScoreUtils::computeEvalue(rawScore, kmnByLen[res.dbLen], lambda);
+                    unsigned int qstart = res.qStartPos;
+                    unsigned int qend = res.qEndPos;
+                    unsigned int qLen = res.qLen;
+                    res.qStartPos = res.dbStartPos;
+                    res.qEndPos = res.dbEndPos;
+                    res.qLen = res.dbLen;
+                    res.dbStartPos = qstart;
+                    res.dbEndPos = qend;
+                    res.dbLen = qLen;
+                }
+
+                std::stable_sort(alnRes.begin(), alnRes.end(), Matcher::compareHits);
+
+                std::ostringstream swResultsSs;
+                for (size_t j = 0; j < alnRes.size(); j++) {
+                    bool addBacktrace = Matcher::ALN_RES_WITH_BT_COL_CNT == cols;
+                    swResultsSs << Matcher::resultToString(alnRes[j], addBacktrace);
+                }
+                result = swResultsSs.str();
+            }
+        }
+
+        splitWriter.write(result.c_str(), result.size(), SSTR(id).c_str(), thread_idx);
+
+        delete swaps[i].second;
+    }
+
+    Debug(Debug::INFO) << "Done.\n";
+    splitWriter.close();
+
+    delete[] kmnByLen;
+
+    return EXIT_SUCCESS;
+}
 
 typedef std::map<unsigned int, std::string *> SwapIt;
 
@@ -67,14 +163,7 @@ void processSplit(DBReader<unsigned int> &dbr,
             // extract key from results (ids must be always at the first position)
             Util::parseKey(data, dbKey);
             unsigned int key = (unsigned int) strtoul(dbKey, NULL, 10);
-            std::string *entry = NULL;
-            SwapIt::const_iterator it = map.find(key);
-            if (it != map.end()) {
-                entry = it->second;
-            } else {
-                entry = new std::string();
-                map.emplace(key, entry);
-            }
+            std::string *entry = map[key];
             // write data to map
             entry->append(outerKey);
             // next db key
@@ -85,125 +174,33 @@ void processSplit(DBReader<unsigned int> &dbr,
     }
 }
 
-int doSwap(Parameters &par, DBReader<unsigned int> &rdbr,
-           const std::pair<std::string, std::string> &resultdb,
-           const size_t startIndex, const size_t domainSize) {
-#ifdef OPENMP
-    omp_set_num_threads(par.threads);
-#endif
+std::vector<std::pair<unsigned int, std::string*>> readSwap(const char* dataFile, const char* indexFile) {
+    DBReader<unsigned int> reader(dataFile, indexFile);
+    reader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
 
-    DBReader<unsigned int> qdbr(par.db1.c_str(), par.db1Index.c_str());
-    qdbr.open(DBReader<unsigned int>::NOSORT);
-    DBReader<unsigned int> tdbr(par.db2.c_str(), par.db2Index.c_str());
-    tdbr.open(DBReader<unsigned int>::NOSORT);
-
-    SubstitutionMatrix subMat(par.scoringMatrixFile.c_str(), 2.0, 0.0);
-    BlastScoreUtils::BlastStat stats = BlastScoreUtils::getAltschulStatsForMatrix(subMat.getMatrixName(),
-                                                                                  Matcher::GAP_OPEN,
-                                                                                  Matcher::GAP_EXTEND);
-
-    int seqLen = static_cast<int>(par.maxSeqLen);
-    double *kmnByLen = new double[seqLen];
-    for (int len = 0; len < seqLen; len++) {
-        kmnByLen[len] = BlastScoreUtils::computeKmn(len, stats.K, stats.lambda, stats.alpha, stats.beta,
-                                                    qdbr.getAminoAcidDBSize(), qdbr.getSize());
-    }
-    const double logK = log(stats.K);
-    const double lambdaLog2 = stats.lambda / log(2.0);
-    const double logKLog2 = logK / log(2.0);
-
+    std::vector<std::pair<unsigned int, std::string*>> result;
     // read all keys
-    SwapIt swapMap = readAllKeysIntoMap(rdbr.getDataFileName());
+    SwapIt swapMap = readAllKeysIntoMap(reader.getDataFileName());
 
-    FILE *dataFile = fopen(rdbr.getDataFileName(), "r");
-    processSplit(rdbr, dataFile, swapMap, startIndex, domainSize);
-    fclose(dataFile);
+    FILE *file = fopen(reader.getDataFileName(), "r");
+    processSplit(reader, file, swapMap, 0, reader.getSize());
+    fclose(file);
+    std::copy(swapMap.begin(), swapMap.end(), result.begin());
 
-    // create and open db for writing
-    Debug(Debug::INFO) << "Start to swap results. Write to " << resultdb.first << ".\n";
-    DBWriter splitWriter(resultdb.first.c_str(), resultdb.second.c_str(),
-                         static_cast<unsigned int>(par.threads));
-    splitWriter.open();
+    reader.close();
 
-    // Write sorted results and delete memory
-    char *wordCnt[255];
-#pragma omp parallel
-    {     
-        unsigned int thread_idx = 0;
-        unsigned int thread_cnt = 1;
-#ifdef OPENMP
-        thread_idx = static_cast<unsigned int>(omp_get_thread_num());
-        thread_cnt = static_cast<unsigned int>(omp_get_num_threads());
-#endif
-        size_t size = swapMap.size();
-        SwapIt::const_iterator it = swapMap.begin();
-        std::advance(it, thread_idx);
-
-        for (unsigned int i = thread_idx; i < size; i+= thread_cnt) {
-            unsigned int id = it->first;
-            std::string result = *it->second;
-            if (result.size() > 1) {
-                size_t cols = Util::getWordsOfLine((char *) result.c_str(), wordCnt, 254);
-                if (Matcher::ALN_RES_WITH_OUT_BT_COL_CNT >= cols) {
-                    std::vector<Matcher::result_t> alnRes = Matcher::readAlignmentResults((char *) result.c_str());
-                    for (size_t i = 0; i < alnRes.size(); i++) {
-                        Matcher::result_t &res = alnRes[i];
-                        double rawScore = BlastScoreUtils::bitScoreToRawScore(res.score, lambdaLog2, logKLog2);
-                        res.eval = BlastScoreUtils::computeEvalue(rawScore, kmnByLen[res.dbLen], stats.lambda);
-                        unsigned int qstart = res.qStartPos;
-                        unsigned int qend = res.qEndPos;
-                        unsigned int qLen = res.qLen;
-                        res.qStartPos = res.dbStartPos;
-                        res.qEndPos = res.dbEndPos;
-                        res.qLen = res.dbLen;
-                        res.dbStartPos = qstart;
-                        res.dbEndPos = qend;
-                        res.dbLen = qLen;
-                    }
-
-                    std::stable_sort(alnRes.begin(), alnRes.end(), Matcher::compareHits);
-
-                    std::ostringstream swResultsSs;
-                    for (size_t i = 0; i < alnRes.size(); i++) {
-                        bool addBacktrace = Matcher::ALN_RES_WITH_BT_COL_CNT == cols;
-                        swResultsSs << Matcher::resultToString(alnRes[i], addBacktrace);
-                    }
-                    result = swResultsSs.str();
-                }
-            }
-
-            splitWriter.write(result.c_str(), result.size(), SSTR(id).c_str(), thread_idx);
-
-            // remove just the value (string *)
-            delete it->second;
-
-            if (i + thread_cnt < size) {
-                std::advance(it, thread_cnt);
-            }
-        }
-    }
-
-    Debug(Debug::INFO) << "Done.\n";
-    splitWriter.close();
-
-    qdbr.close();
-    tdbr.close();
-    delete[] kmnByLen;
-
-    return EXIT_SUCCESS;
+    return result;
 }
 
 int doSwap(Parameters &par, const unsigned int mpiRank, const unsigned int mpiNumProc) {
-    DBReader<unsigned int> reader(par.db3.c_str(), par.db3Index.c_str());
-    reader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
+    std::vector<std::pair<unsigned int, std::string*>> swap = readSwap(par.db3.c_str(), par.db3Index.c_str());
 
     size_t dbFrom = 0;
     size_t dbSize = 0;
-    Util::decomposeDomain(reader.getSize(), mpiRank, mpiNumProc, &dbFrom, &dbSize);
-    std::pair<std::string, std::string> tmpOutput = Util::createTmpFileNames(par.db4, par.db4Index, mpiRank);
+    Util::decomposeDomain(swap.size(), mpiRank, mpiNumProc, &dbFrom, &dbSize);
 
-    int status = doSwap(par, reader, tmpOutput, dbFrom, dbSize);
-    reader.close();
+    std::pair<std::string, std::string> tmpOutput = Util::createTmpFileNames(par.db4, par.db4Index, mpiRank);
+    int status = doSwap(par, swap, tmpOutput, dbFrom, dbSize);
 
 #ifdef HAVE_MPI
     MPI_Barrier(MPI_COMM_WORLD);
@@ -213,32 +210,26 @@ int doSwap(Parameters &par, const unsigned int mpiRank, const unsigned int mpiNu
     if (mpiRank == 0) {
         std::vector<std::pair<std::string, std::string>> splitFiles;
         for (unsigned int proc = 0; proc < mpiNumProc; ++proc) {
-            std::pair<std::string, std::string> names = Util::createTmpFileNames(par.db4, par.db4Index, proc);
-            splitFiles.push_back(std::make_pair(names.first, names.second));
+            splitFiles.push_back(Util::createTmpFileNames(par.db4, par.db4Index, proc));
         }
-        Prefiltering::mergeOutput(par.db4, par.db4, splitFiles);
+        Alignment::mergeAndRemoveTmpDatabases(par.db4, par.db4Index, splitFiles);
     }
 
     return status;
 }
 
 int doSwap(Parameters &par) {
-    size_t resultSize;
+    std::vector<std::pair<unsigned int, std::string*>> swap = readSwap(par.db3.c_str(), par.db3Index.c_str());
 
-    DBReader<unsigned int> reader(par.db3.c_str(), par.db3Index.c_str());
-    reader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
-    resultSize = reader.getSize();
-
-    int status = doSwap(par, reader, std::make_pair(par.db4, par.db4Index), 0, resultSize);
-    reader.close();
+    std::pair<std::string, std::string> out = std::make_pair(par.db4, par.db4Index);
+    int status = doSwap(par, swap, out, 0, swap.size());
 
     return status;
 }
 
 int swapresults(int argc, const char *argv[]) {
-#if ENABLE_MPI_SWAP
     MMseqsMPI::init(argc, argv);
-#endif
+
     std::string usage("Swaps results of ffindex database. A -> A, B, C to A->A, B->A, C->A \n");
     usage.append("Written by Martin Steinegger (martin.steinegger@mpibpc.mpg.de).\n");
     usage.append("USAGE: <queryDb> <targetDb> <ffindexDB> <fastaDB>\n");
@@ -246,13 +237,13 @@ int swapresults(int argc, const char *argv[]) {
     Parameters par;
     par.parseParameters(argc, argv, usage, par.swapresults, 4);
 
-#if defined(HAVE_MPI) && ENABLE_MPI_SWAP
+#ifdef HAVE_MPI
     int status = doSwap(par, MMseqsMPI::rank, MMseqsMPI::numProc);
 #else
     int status = doSwap(par);
 #endif
 
-#if defined(HAVE_MPI) && ENABLE_MPI_SWAP
+#ifdef HAVE_MPI
     MPI_Finalize();
 #endif
 
