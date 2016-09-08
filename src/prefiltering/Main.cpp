d188e3f2fefa5a7cfa1ac1220fea71bf92b58a31
Martin Steinegger <themartinsteinegger@gmail.com>
2016-09-08 14:08:36 +0900

    Fix problem if more splits exists than nodes with MPI

--- src/prefiltering/Main.cpp
+++ src/prefiltering/Main.cpp
@@ -1,16 +1,17 @@
+
 #include <iostream>
 #include <string>
 #include <sys/time.h>
 
 #include "Prefiltering.h"
+#include "Util.h"
 #include "Parameters.h"
 
 #include "MMseqsMPI.h"
-
+#define HAVE_MPI 1
 #ifdef OPENMP
 #include <omp.h>
 #endif
-
 int prefilter(int argc, const char **argv)
 {
     MMseqsMPI::init(argc, argv);
@@ -31,24 +32,66 @@ int prefilter(int argc, const char **argv)
 #endif
 
     Debug(Debug::WARNING) << "Initialising data structures...\n";
+#ifdef HAVE_MPI
+    std::pair<std::string, std::string> filenamePair = Util::createTmpFileNames(par.db3, par.db3Index, MMseqsMPI::rank);
+    std::cout << filenamePair.first.c_str() << "\t" <<  filenamePair.second.c_str() << std::endl;
+    Prefiltering* pref = new Prefiltering(par.db1,par.db1Index,
+                                          par.db2,par.db2Index,
+                                          filenamePair.first.c_str(), filenamePair.second.c_str(), par);
+#else
     Prefiltering* pref = new Prefiltering(par.db1,par.db1Index,
             par.db2,par.db2Index,
             par.db3,par.db3Index,par);
-
+#endif
     gettimeofday(&end, NULL);
     int sec = end.tv_sec - start.tv_sec;
     Debug(Debug::WARNING) << "Time for init: " << (sec / 3600) << " h " << (sec % 3600 / 60) << " m " << (sec % 60) << "s\n\n\n";
     gettimeofday(&start, NULL);
+#ifdef HAVE_MPI
+    if(par.split > MMseqsMPI::numProc){
+        // if split size is great than nodes than we have to
+        // distribute all splits equally over all nodes
+        int * splitCntPerProc = new int[MMseqsMPI::numProc];
+        memset(splitCntPerProc, 0, sizeof(int) * MMseqsMPI::numProc);
+        for(int i = 0; i < par.split; i++){
+            splitCntPerProc[i % MMseqsMPI::numProc] += 1;
+        }
+        int fromSplit = 0;
+        for(int i = 0; i < MMseqsMPI::rank; i++){
+            fromSplit += splitCntPerProc[i];
+        }
+        pref->run(fromSplit, splitCntPerProc[MMseqsMPI::rank]);
+        delete [] splitCntPerProc;
+    }if(par.split == 0 ){
+        // if database fits into the memory of a single node split by query.
+        // each node should just compute 1 query split
+        pref->setSplit(MMseqsMPI::numProc);
+        pref->run(MMseqsMPI::rank, 1);
+    }else{
+        // if more nodes exists than splits are needed set split to the amount of nodes
+        // each node needs to compute 1 target split
+        pref->setSplit(MMseqsMPI::numProc);
+        pref->run(MMseqsMPI::rank, 1);
+    }
+#else
+    pref->run(0, (par.split == 0) ?  1 : par.split );
+#endif
+    delete pref;
 
 #ifdef HAVE_MPI
-    pref->run(MMseqsMPI::rank, MMseqsMPI::numProc);
-#else
-    pref->run();
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(MMseqsMPI::rank == 0){
+        std::vector<std::pair<std::string, std::string> > splitFiles;
+        for(int procs = 0; procs < MMseqsMPI::numProc; procs++){
+            splitFiles.push_back(Util::createTmpFileNames(par.db3, par.db3Index, procs));
+        }
+        // merge output ffindex databases
+        Prefiltering::mergeFiles(splitFiles, pref->getSplitMode(), par.db3, par.db3Index);
+    }
 #endif
     gettimeofday(&end, NULL);
     sec = end.tv_sec - start.tv_sec;
     Debug(Debug::WARNING) << "\nOverall time for prefiltering run: " << (sec / 3600) << " h " << (sec % 3600 / 60) << " m " << (sec % 60) << "s\n";
-    delete pref;
 
 #ifdef HAVE_MPI
     MPI_Finalize();
