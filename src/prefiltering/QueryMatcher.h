421eca20e5232d9a80f67fc46fe130a5a02ecd7d
Martin Steinegger <themartinsteinegger@gmail.com>
2016-09-11 20:09:07 +0900

    Refactor prefilter

--- src/prefiltering/QueryMatcher.h
+++ src/prefiltering/QueryMatcher.h
@@ -5,19 +5,47 @@
 #ifndef MMSEQS_QUERYTEMPLATEMATCHEREXACTMATCH_H
 #define MMSEQS_QUERYTEMPLATEMATCHEREXACTMATCH_H
 
+#include <cstdlib>
 #include "CacheFriendlyOperations.h"
-#include "QueryTemplateMatcher.h"
 #include "UngappedAlignment.h"
+#include "KmerGenerator.h"
 
 
-class QueryTemplateLocalFast : public virtual QueryTemplateMatcher {
+struct statistics_t{
+    double kmersPerPos;
+    size_t dbMatches;
+    size_t doubleMatches;
+    size_t querySeqLen;
+    size_t diagonalOverflow;
+    size_t resultsPassedPrefPerSeq;
+    statistics_t() : kmersPerPos(0.0) , dbMatches(0) , doubleMatches(0), querySeqLen(0), diagonalOverflow(0), resultsPassedPrefPerSeq(0) {};
+    statistics_t(double kmersPerPos, size_t dbMatches,
+                 size_t doubleMatches, size_t querySeqLen, size_t diagonalOverflow, size_t resultsPassedPrefPerSeq) : kmersPerPos(kmersPerPos), resultsPassedPrefPerSeq(resultsPassedPrefPerSeq){};
+};
+
+struct hit_t {
+    unsigned int seqId;
+    float pScore;
+    unsigned short diagonal;
+    unsigned char prefScore;
+
+    static bool compareHitsByPValue(hit_t first, hit_t second){
+        return (first.pScore > second.pScore) ? true : false;
+    }
+
+    static bool compareHitsByDiagonalScore(hit_t first, hit_t second){
+        return (first.prefScore > second.prefScore) ? true : false;
+    }
+};
+
+class QueryMatcher {
 public:
-    QueryTemplateLocalFast(BaseMatrix *m, IndexTable *indexTable,
+    QueryMatcher(BaseMatrix *m, IndexTable *indexTable,
                            unsigned int *seqLens, short kmerThr,
                            double kmerMatchProb, int kmerSize, size_t dbSize,
                            unsigned int maxSeqLen, unsigned int effectiveKmerSize,
                            size_t maxHitsPerQuery, bool aaBiasCorrection, bool diagonalScoring, unsigned int minDiagScoreThr);
-    ~QueryTemplateLocalFast();
+    ~QueryMatcher();
 
     // returns result for the sequence
     // identityId is the id of the identitical sequence in the target database if there is any, UINT_MAX otherwise
@@ -28,7 +56,65 @@ public:
                         size_t outputSize, unsigned short indexFrom, unsigned short indexTo, bool computeTotalScore);
 
     void updateScoreBins(CounterResult *result, size_t elementCount);
+
+    // set substituion matrix for KmerGenerator
+    void setProfileMatrix(ScoreMatrix **matrix){
+        this->kmerGenerator->setDivideStrategy(matrix );
+    }
+
+    // set substitution matrix
+    void setSubstitutionMatrix(ScoreMatrix * three, ScoreMatrix * two) {
+        this->kmerGenerator->setDivideStrategy(three, two );
+    }
+
+    // get statistics
+    const statistics_t * getStatistics(){
+        return stats;
+    }
+
+    const static size_t SCORE_RANGE = 256;
+
+    static unsigned int computeScoreThreshold(unsigned int * scoreSizes, size_t maxHitsPerQuery) {
+        size_t foundHits = 0;
+        size_t scoreThr = 0;
+        for(scoreThr = SCORE_RANGE - 1; scoreThr > 0 ; scoreThr--){
+            foundHits += scoreSizes[scoreThr];
+            if(foundHits >= maxHitsPerQuery)
+                break;
+        }
+        return scoreThr;
+    }
+
+// compute -log(p)
+static inline double computeLogProbability(const unsigned short rawScore, const unsigned int dbSeqLen,
+                                           const double kmerMatchProb, const double logMatchProb,
+                                           const double logScoreFactorial) {
+    const double score = static_cast<double>(rawScore);
+    const double dbSeqLenDbl = static_cast<double>(dbSeqLen);
+    const double mu = kmerMatchProb * dbSeqLenDbl;
+    const double mid_term = score * (logMatchProb + log(dbSeqLenDbl));
+    const double first_term = -(mu * score /(score + 1));
+    return first_term + mid_term - logScoreFactorial;
+}
+
 protected:
+
+    // keeps stats for run
+    statistics_t * stats;
+
+    // scoring matrix for local amino acid bias correction
+    BaseMatrix * m;
+    /* generates kmer lists */
+    KmerGenerator * kmerGenerator;
+    /* contains the sequences for a kmer */
+    IndexTable * indexTable;
+    // k of the k-mer
+    int kmerSize;
+    // local amino acid bias correction
+    bool aaBiasCorrection;
+
+    // kmer threshold for kmer generator
+    short kmerThr;
 
     unsigned int maxDbMatches;
     unsigned int dbSize;
@@ -115,6 +201,8 @@ protected:
     size_t mergeElements(bool diagonalScoring, CounterResult *foundDiagonals, size_t hitCounter);
 
     size_t keepMaxScoreElementOnly(CounterResult *foundDiagonals, size_t resultSize);
+
+    const static size_t MAX_RES_LIST_LEN = 150000;
 };
 
 #endif //MMSEQS_QUERYTEMPLATEMATCHEREXACTMATCH_H
