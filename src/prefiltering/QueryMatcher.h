3bfeccad5c9478acf307323e975bdeac5d112485
mad <mad@Martins-MacBook-Pro.local>
2016-09-13 23:23:22 +0900

    Refactor after bug fix

--- src/prefiltering/QueryMatcher.h
+++ src/prefiltering/QueryMatcher.h
@@ -20,7 +20,12 @@ struct statistics_t{
     size_t resultsPassedPrefPerSeq;
     statistics_t() : kmersPerPos(0.0) , dbMatches(0) , doubleMatches(0), querySeqLen(0), diagonalOverflow(0), resultsPassedPrefPerSeq(0) {};
     statistics_t(double kmersPerPos, size_t dbMatches,
-                 size_t doubleMatches, size_t querySeqLen, size_t diagonalOverflow, size_t resultsPassedPrefPerSeq) : kmersPerPos(kmersPerPos), resultsPassedPrefPerSeq(resultsPassedPrefPerSeq){};
+                 size_t doubleMatches, size_t querySeqLen, size_t diagonalOverflow, size_t resultsPassedPrefPerSeq) : kmersPerPos(kmersPerPos),
+                                                                                                                      dbMatches(dbMatches),
+                                                                                                                      doubleMatches(doubleMatches),
+                                                                                                                      querySeqLen(querySeqLen),
+                                                                                                                      diagonalOverflow(diagonalOverflow),
+                                                                                                                      resultsPassedPrefPerSeq(resultsPassedPrefPerSeq){};
 };
 
 struct hit_t {
@@ -107,7 +112,7 @@ protected:
     /* generates kmer lists */
     KmerGenerator * kmerGenerator;
     /* contains the sequences for a kmer */
-    IndexTable * indexTable;
+IndexTable * indexTable;
     // k of the k-mer
     int kmerSize;
     // local amino acid bias correction
