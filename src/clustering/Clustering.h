f11fa0cde0513ff5c7ad4768f8a53bd11bd8f54c
Milot Mirdita <milot@mirdita.de>
2016-09-14 13:06:33 +0200

    Cleanup clustering caller code

--- src/clustering/Clustering.h
+++ src/clustering/Clustering.h
@@ -10,37 +10,36 @@
 #include "SetElement.h"
 
 class Clustering {
+public:
+    Clustering(const std::string &seqDB, const std::string &seqDBIndex,
+               const std::string &alnResultsDB, const std::string &alnResultsDBIndex,
+               const std::string &outDB, const std::string &outDBIndex,
+               int validateClustering, unsigned int maxIteration,
+               int similarityScoreType, int threads);
 
-    public:
-
-        Clustering (std::string seqDB, std::string seqDBIndex,
-                std::string alnResultsDB, std::string alnResultsDBIndex,
-                std::string outDB, std::string outDBIndex, 
-                int validateClustering,  unsigned int maxIteration,
-                int similarityScoreType, int threads);
-
-               void run(int mode);
+    void run(int mode);
 
 
-        ~Clustering();
-    private:
-        // check if every element is member in only one cluster
-        bool validate_result(std::list<set *> * ret,unsigned int uniqu_element_count);
+    ~Clustering();
 
+private:
+    // check if every element is member in only one cluster
+    bool validate_result(std::list<set *> *ret, unsigned int uniqu_element_count);
 
+    void writeData(DBWriter *dbw, const std::map<unsigned int, std::vector<unsigned int>> &ret);
 
-        void writeData(DBWriter *dbw, std::map<unsigned int, std::vector<unsigned int> > ret);
+    DBReader<unsigned int> *seqDbr;
+    DBReader<unsigned int> *alnDbr;
 
-        DBReader<unsigned int>* seqDbr;
-        DBReader<unsigned int>* alnDbr;
+    int validate;
 
-        int validate;
+    //values for affinity clustering
+    unsigned int maxIteration;
+    int similarityScoreType;
 
-        //values for affinity clustering
-        unsigned int maxIteration;
-        int similarityScoreType;
-        int threads;
-        std::string outDBIndex;
-        std::string outDB;
+    int threads;
+    std::string outDB;
+    std::string outDBIndex;
 };
+
 #endif
