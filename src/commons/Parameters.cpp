37c29aaad56e8bda53e543439a3163a83106103c
Martin Steinegger <themartinsteinegger@gmail.com>
2016-10-04 23:24:23 +0900

    New max thread detection since omp_num_max_threads does not work properly

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -2,6 +2,7 @@
 #include "Sequence.h"
 #include "Debug.h"
 #include "Util.h"
+#include <unistd.h>
 
 #include <iomanip>
 #include <regex.h>
@@ -765,11 +766,16 @@ void Parameters::setDefaults() {
     
     threads = 1;
 #ifdef OPENMP
-    threads = omp_get_max_threads();
+#ifdef _SC_NPROCESSORS_ONLN
+    threads = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+    if(threads  <= 1){
+        threads = Util::omp_thread_count();
+    }
 #endif
     compBiasCorrection = 1;
     diagonalScoring = 1;
-    minDiagScoreThr = 30;
+    minDiagScoreThr = 15;
     spacedKmer = true;
     profile = false;
     nucl = false;
