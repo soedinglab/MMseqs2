31b925a8cc0e213c1498c7f3ae386299ac964de6
Milot Mirdita <milot@mirdita.de>
2017-01-02 21:01:09 +0700

    Check that tmp directory exists for updating and only write the script if it doesnt exist

--- src/workflow/ClusterUpdate.cpp
+++ src/workflow/ClusterUpdate.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <Debug.h>
 
 #include "Parameters.h"
 #include "FileUtil.h"
@@ -20,7 +21,18 @@ int clusterupdate(int argc, const char **argv, const Command& command) {
     cmd.addVariable("SEARCH_PAR", par.createParameterString(par.clusterUpdateSearch).c_str());
     cmd.addVariable("CLUST_PAR", par.createParameterString(par.clusterUpdateClust).c_str());
 
-    FileUtil::writeFile(par.db5 + "/update_clustering.sh", update_clustering_sh, update_clustering_sh_len);
+    std::string scriptPath(par.db5);
+    if(!FileUtil::directoryExists(par.db5.c_str())) {
+        Debug(Debug::ERROR) << "Tmp directory " << par.db5 << " not found!\n";
+        return EXIT_FAILURE;
+    }
+
+    scriptPath.append("/update_clustering.sh");
+    if(!FileUtil::fileExists(scriptPath.c_str())) {
+        FileUtil::writeFile(scriptPath, update_clustering_sh, update_clustering_sh_len);
+    } else {
+        Debug(Debug::INFO) << "Update script already exists. Skipped overwriting.\n";
+    }
     std::string program(par.db5 + "/update_clustering.sh");
 	cmd.execProgram(program.c_str(), 5, argv);
 
