f15fe01abd2f2b2fbd192b020df0bfef13654d88
Milot Mirdita <milot@mirdita.de>
2017-01-04 14:43:52 +0700

    Fix small memory leak

--- src/util/convertfiles.cpp
+++ src/util/convertfiles.cpp
@@ -50,12 +50,13 @@ void convertfiles::convertFfindexToTsv(std::string clusteringfile, std::string s
     outfile_stream << "algorithm\tclusterid\tid2\n";
     outfile_stream_cluster_summary << "algorithm\tclusternumber\tsingletons\n";
     int singletons = 0;
+    char *idbuffer1 = new char[255 + 1];
+
     for (size_t i = 0; i < cluster_ffindex_reader.getSize(); ++i) {
 
         int clustersize = 0;
         char *data = cluster_ffindex_reader.getData(i);
         std::string idbuffer = "";
-        char *idbuffer1 = new char[255 + 1];
         std::string representative = "";
         while (*data != '\0') {
             Util::parseKey(data, idbuffer1);
@@ -79,6 +80,8 @@ void convertfiles::convertFfindexToTsv(std::string clusteringfile, std::string s
         outfile_stream_clustersize << suffix << "\t" << representative << "\t" << clustersize << "\n";
     }
     outfile_stream_cluster_summary << suffix << "\t" << cluster_ffindex_reader.getSize() << "\t" << singletons << "\n";
+
+    delete[] idbuffer1;
 
     cluster_ffindex_reader.close();
 
