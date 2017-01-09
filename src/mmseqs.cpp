4413cbd5bb654162cb8fc5d289933498a1ebda16
Clovis Galiez <cgaliez@gwdu103.global.gwdg.cluster>
2017-01-09 11:42:02 +0100

    Threads param for swapresults

--- src/mmseqs.cpp
+++ src/mmseqs.cpp
@@ -130,7 +130,7 @@ static struct Command commands[] = {
             "Milot Mirdita <milot@mirdita.de>",
             "<i:sequenceDB> <o:sequenceDB>",
             CITATION_MMSEQS2},
-        {"swapresults",          swapresults,          &par.empty,                COMMAND_DB,
+        {"swapresults",          swapresults,          &par.swapresults,                COMMAND_DB,
             "Reformat prefilter/alignment/cluster DB as if target DB had been searched through query DB",
             NULL,
             "Martin Steinegger <martin.steinegger@mpibpc.mpg.de> & Clovis Galiez",
