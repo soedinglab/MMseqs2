d853a5498216f4fcbc007e0cf7294b849c7e6091
Milot Mirdita <milot@mirdita.de>
2017-01-09 21:19:26 +0700

    use max-accept instead of max-seqs to avoid the sensitivity loss

--- data/update_clustering.sh
+++ data/update_clustering.sh
@@ -198,7 +198,7 @@ echo "========= previous (rep seq of) clusters =========="
 echo "==================================================="
 mkdir -p "$TMP/search"
 if notExists "$TMP/newSeqsHits"; then
-    $RUNNER $MMSEQS search "$TMP/NEWDB.newSeqs" "$TMP/OLDDB.repSeq" "$TMP/newSeqsHits" "$TMP/search" --max-seqs 1 ${SEARCH_PAR} \
+    $RUNNER $MMSEQS search "$TMP/NEWDB.newSeqs" "$TMP/OLDDB.repSeq" "$TMP/newSeqsHits" "$TMP/search" --max-accept 1 ${SEARCH_PAR} \
         || fail "Search died"
 fi
 
