3ef0d5831ba8463728fa0943052aebcf4a43aa82
Martin Steinegger <themartinsteinegger@gmail.com>
2017-01-23 18:23:59 +0900

    Improve DBReader/DBWriter performance and memory consumption

--- src/commons/DBReader.cpp
+++ src/commons/DBReader.cpp
@@ -89,7 +89,7 @@ void DBReader<std::string>::sortIndex() {
         for (size_t i = 0; i < size; i++) {
             sortArray[i] = std::make_pair(index[i], seqLens[i]);
         }
-        std::stable_sort(sortArray, sortArray + size, compareIndexLengthPairById());
+        omptl::sort(sortArray, sortArray + size, compareIndexLengthPairById());
         for (size_t i = 0; i < size; ++i) {
             index[i].id = sortArray[i].first.id;
             index[i].offset = sortArray[i].first.offset;
@@ -115,7 +115,7 @@ void DBReader<unsigned int>::sortIndex() {
     for (size_t i = 0; i < size; i++) {
         sortArray[i] = std::make_pair(index[i], std::make_pair(i,seqLens[i]));
     }
-    std::stable_sort(sortArray, sortArray + size, compareIndexLengthPairByIdKeepTrack());
+    omptl::sort(sortArray, sortArray + size, compareIndexLengthPairByIdKeepTrack());
     for (size_t i = 0; i < size; ++i) {
         index[i].id = sortArray[i].first.id;
         index[i].offset = sortArray[i].first.offset;
@@ -134,6 +134,7 @@ void DBReader<unsigned int>::sortIndex() {
             local2id[i] = i;
             sortForMapping[i] = std::make_pair(i, seqLens[i]);
         }
+        //this sort has to be stable to assure same clustering results
         std::stable_sort(sortForMapping, sortForMapping + size, comparePairBySeqLength());
         for (size_t i = 0; i < size; i++) {
             id2local[sortForMapping[i].first] = i;
@@ -151,7 +152,7 @@ void DBReader<unsigned int>::sortIndex() {
             local2id[i] = i;
             sortForMapping[i] = std::make_pair(i, index[i].offset);
         }
-        std::stable_sort(sortForMapping, sortForMapping + size, comparePairByOffset());
+        omptl::sort(sortForMapping, sortForMapping + size, comparePairByOffset());
         for (size_t i = 0; i < size; i++) {
             id2local[sortForMapping[i].first] = i;
             local2id[i] = sortForMapping[i].first;
