dc90e8467f49f676267238f63c6ff3dad26e4899
Milot Mirdita <milot@mirdita.de>
2017-02-13 20:27:52 +0700

    Fix cov/targetCov issue in updating

--- src/workflow/ClusterUpdate.cpp
+++ src/workflow/ClusterUpdate.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
-#include <Debug.h>
 
+#include "Debug.h"
+#include "Util.h"
 #include "Parameters.h"
 #include "FileUtil.h"
 #include "CommandCaller.h"
@@ -10,6 +11,31 @@
 int clusterupdate(int argc, const char **argv, const Command& command) {
     Parameters& par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, 5);
+
+    bool targetCov = false;
+    bool cov = false;
+    for (size_t i = 0; i < par.clusterUpdate.size(); i++) {
+        if (par.clusterUpdate[i].uniqid == Parameters::PARAM_TARGET_COV_ID && par.clusterUpdate[i].wasSet) {
+            targetCov = true;
+        }
+        if (par.clusterUpdate[i].uniqid == Parameters::PARAM_C_ID && par.clusterUpdate[i].wasSet) {
+            cov = true;
+        }
+    }
+    if (cov && targetCov) {
+        Debug(Debug::ERROR) << "The paramter -c can not be combined with --target-cov.\n";
+        EXIT(EXIT_FAILURE);
+    }
+
+    std::vector<MMseqsParameter> clustParams;
+    for (std::vector<MMseqsParameter>::iterator it = par.clusterUpdateClust.begin(); it != par.clusterUpdateClust.end(); ++it) {
+        if ((cov && (*it).uniqid == Parameters::PARAM_TARGET_COV_ID)
+                || (targetCov && (*it).uniqid == Parameters::PARAM_C_ID)) {
+            continue;
+        }
+        clustParams.push_back(*it);
+    }
+
 
     CommandCaller cmd;
     if(par.removeTmpFiles) {
@@ -23,7 +49,7 @@ int clusterupdate(int argc, const char **argv, const Command& command) {
     cmd.addVariable("RUNNER", par.runner.c_str());
     cmd.addVariable("DIFF_PAR", par.createParameterString(par.diff).c_str());
     cmd.addVariable("SEARCH_PAR", par.createParameterString(par.clusterUpdateSearch).c_str());
-    cmd.addVariable("CLUST_PAR", par.createParameterString(par.clusterUpdateClust).c_str());
+    cmd.addVariable("CLUST_PAR", par.createParameterString(clustParams).c_str());
 
     std::string scriptPath(par.db5);
     if(!FileUtil::directoryExists(par.db5.c_str())) {
