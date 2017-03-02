f53e56a59e595dda70039a2192eca9b890d901d6
Martin Steinegger <themartinsteinegger@gmail.com>
2017-03-02 17:42:25 +0900

    target-cov milot fix

--- src/workflow/ClusterUpdate.cpp
+++ src/workflow/ClusterUpdate.cpp
@@ -15,6 +15,31 @@ int clusterupdate(int argc, const char **argv, const Command& command) {
     Parameters& par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, 5);
 
+    bool targetCov = false;
+    bool cov = false;
+    for (size_t i = 0; i < par.clusterUpdate.size(); i++) {
+        if (par.clusterUpdate[i].uniqid == Parameters::PARAM_TARGET_COV_ID && par.clusterUpdate[i].wasSet) {
+            targetCov = true;
+        }
+        if (par.clusterUpdate[i].uniqid == Parameters::PARAM_C_ID && par.clusterUpdate[i].wasSet) {
+            cov = true;
+        }
+    }
+    if (cov && targetCov) {
+        Debug(Debug::ERROR) << "The paramter -c can not be combined with --target-cov.\n";
+        EXIT(EXIT_FAILURE);
+    }
+
+    std::vector<MMseqsParameter> clustParams;
+    for (std::vector<MMseqsParameter>::iterator it = par.clusterUpdateClust.begin(); it != par.clusterUpdateClust.end(); ++it) {
+        if ((cov && (*it).uniqid == Parameters::PARAM_TARGET_COV_ID)
+                || (targetCov && (*it).uniqid == Parameters::PARAM_C_ID)) {
+            continue;
+        }
+        clustParams.push_back(*it);
+    }
+
+
     CommandCaller cmd;
     if(par.removeTmpFiles) {
         cmd.addVariable("REMOVE_TMP", "TRUE");
