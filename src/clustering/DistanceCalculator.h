cf3e666c43b2f7beda98a4bdc68014c8f66f8936
Martin Steinegger <themartinsteinegger@gmail.com>
2017-04-15 19:37:39 +0200

    Move functions to header in  DistanceCalculator

--- src/clustering/DistanceCalculator.h
+++ src/clustering/DistanceCalculator.h
@@ -181,9 +181,37 @@ public:
         return score;
     }
 
-    void prepareGlobalAliParam(const BaseMatrix &subMat);
-    double getPvalGlobalAli(float score, size_t len);
-    
+    void prepareGlobalAliParam(const BaseMatrix &subMat)
+    {
+        globalAliMu = 0;
+        globalAliSigma = 0;
+
+        for (size_t i = 0; i<subMat.alphabetSize - 1;i++)
+        {
+
+            for (size_t j = 0; j<subMat.alphabetSize - 1;j++)
+            {
+                globalAliMu += subMat.pBack[i] * subMat.pBack[j] * subMat.subMatrix[i][j];
+            }
+        }
+
+        for (size_t i = 0; i<subMat.alphabetSize - 1;i++)
+        {
+
+            for (size_t j = 0; j<subMat.alphabetSize - 1;j++)
+            {
+                double distToMean = (subMat.subMatrix[i][j] - globalAliMu);
+                globalAliSigma += subMat.pBack[i] * subMat.pBack[j] * distToMean*distToMean;
+            }
+        }
+        globalAliSigma = sqrt(globalAliSigma);
+
+    }
+
+    double getPvalGlobalAli(float score,size_t len)
+    {
+        return 0.5 - 0.5*erf((score/len-globalAliMu)/(sqrt(2.0/sqrt((float)len))*globalAliSigma));
+    }
 private:
     
     float globalAliMu,globalAliSigma;
