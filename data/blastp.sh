deebfdff632480eed4336cab6b026373c47a59a3
Milot Mirdita <milot@mirdita.de>
2017-10-27 14:16:50 +0200

    Remove some more bash-isms

--- data/blastp.sh
+++ data/blastp.sh
@@ -69,7 +69,7 @@ while [ $STEP -lt $STEPS ]; do
                 || fail "Awk step $SENS died"
         fi
 
-        if [[ ! -s "$TMP_PATH/order_step$SENS" ]]; then break; fi
+        if [ ! -s "$TMP_PATH/order_step$SENS" ]; then break; fi
 
         if notExists "$NEXTINPUT"; then
             $MMSEQS createsubdb "$TMP_PATH/order_step$SENS" "$INPUT" "$NEXTINPUT" \
