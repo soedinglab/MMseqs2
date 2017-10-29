f1307fed7fda354ba3a1d126960de17e95ef70e9
Martin Steinegger <themartinsteinegger@gmail.com>
2017-10-29 19:06:27 +0900

    Script to generate github release

---
+++ util/make-release.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -ex
+COMMIT="$1"
+RELEASE_ID="$2"
+RELEASE_MSG="$3"
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+	echo "Please set GitHub Token"
+  exit 1
+fi
+
+function hasCommand() {
+	command -v "$1" >/dev/null 2>&1 || { echo "Please make sure that $1 is in \$PATH."; exit 1; }
+}
+
+hasCommand github-release
+hasCommand echo
+hasCommand date
+hasCommand wget
+
+# download static binaries
+wget -O "mmseqs-static_sse41.tar.gz" "https://mmseqs.com/archive/${COMMIT}/mmseqs-static_sse41.tar.gz"
+wget -O "mmseqs-static_avx2.tar.gz" "https://mmseqs.com/archive/${COMMIT}/mmseqs-static_avx2.tar.gz" 
+
+# create release tag 
+git tag  "${RELEASE_ID}" && git push --tags
+
+# create a formal release
+github-release release \
+    --user soedinglab \
+    --repo mmseqs2 \
+    --tag "${RELEASE_ID}" \
+    --name "MMseqs2 Release $RELEASE_ID" \
+    --description "$RELEASE_MSG" \
+    --pre-release
+
+# upload SSE4.1 static binary
+github-release upload \
+    --user soedinglab \
+    --repo mmseqs2 \
+    --tag "${RELEASE_ID}" \
+    --name "MMseqs2-Linux-SSE4_1.tar.gz" \
+    --file mmseqs-static_sse41.tar.gz
+
+# upload AVX2 static binary
+github-release upload \
+    --user soedinglab \
+    --repo mmseqs2 \
+    --tag "${RELEASE_ID}" \
+    --name "MMseqs2-Linux-AVX2.tar.gz" \
+    --file mmseqs-static_avx2.tar.gz
+    
