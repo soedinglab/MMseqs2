04faac4977a94d5ea6bf65b6e20d9963a1a0b07f
Milot Mirdita <milot@mirdita.de>
2017-11-03 16:31:37 +0100

    Fix TestTaxonomy with for previous commit

--- src/test/TestTaxonomy.cpp
+++ src/test/TestTaxonomy.cpp
@@ -2,7 +2,10 @@
 #include "Debug.h"
 
 int main (int argc, const char **argv) {
-    NcbiTaxonomy t("/Users/mirdita/tmp/taxonomy/names.dmp", "/Users/mirdita/tmp/taxonomy/nodes.dmp");
+    NcbiTaxonomy t("/Users/mirdita/tmp/taxonomy/names.dmp",
+                   "/Users/mirdita/tmp/taxonomy/nodes.dmp",
+                   "/Users/mirdita/tmp/taxonomy/merged.dmp",
+                   "/Users/mirdita/tmp/taxonomy/delnodes.dmp");
     std::vector<int> taxa;
     taxa.push_back(9);
     taxa.push_back(7);
