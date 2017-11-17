6c80e2870d39942bb09ef9f1221bc88b9df3f0a9
Milot Mirdita <milot@mirdita.de>
2017-11-17 13:23:54 +0100

    make profile2pssm fast

--- src/util/profile2pssm.cpp
+++ src/util/profile2pssm.cpp
@@ -5,6 +5,7 @@
 #include "Debug.h"
 #include "Sequence.h"
 #include "SubstitutionMatrix.h"
+#include "itoa.h"
 
 #ifdef OPENMP
 #include <omp.h>
@@ -19,11 +20,7 @@ int profile2pssm(int argc, const char **argv, const Command &command) {
 #endif
 
     DBReader<unsigned int> profileReader(par.db1.c_str(), par.db1Index.c_str());
-    profileReader.open(DBReader<unsigned int>::NOSORT);
-
-    DBReader<unsigned int> consReader((par.db1 + "_consensus").c_str(),
-                                      (par.db1 + "_consensus.index").c_str());
-    consReader.open(DBReader<unsigned int>::NOSORT);
+    profileReader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
 
     DBWriter writer(par.db2.c_str(), par.db2Index.c_str(), par.threads);
     writer.open();
@@ -33,72 +30,66 @@ int profile2pssm(int argc, const char **argv, const Command &command) {
     size_t entries = profileReader.getSize();
 
     SubstitutionMatrix subMat(par.scoringMatrixFile.c_str(), 2.0f, 0.0);
-#define APPEND(x) do { std::string __x__ = (x); writer.writeAdd(__x__.c_str(), __x__.length(), thread_idx); } while (false);
     Debug(Debug::INFO) << "Start converting profiles.\n";
 #pragma omp parallel
     {
         Sequence seq(par.maxSeqLen, subMat.aa2int, subMat.int2aa, Sequence::HMM_PROFILE, 0, false, par.compBiasCorrection);
 
-#pragma omp for schedule(dynamic, 100)
-        for (size_t i = 0; i < entries; ++i) {
-            Debug::printProgress(i);
-
-            unsigned int thread_idx = 0;
+        unsigned int thread_idx = 0;
 #ifdef OPENMP
-            thread_idx = static_cast<unsigned int>(omp_get_thread_num());
+        thread_idx = static_cast<unsigned int>(omp_get_thread_num());
 #endif
 
+        char* buffer = new char[64];
+        std::string result;
+        result.reserve(10 * 1024 * 1024);
+
+#pragma omp for schedule(dynamic, 1000)
+        for (size_t i = 0; i < entries; ++i) {
+            Debug::printProgress(i);
+            result.clear();
+
             unsigned int key = profileReader.getDbKey(i);
-            char *data = profileReader.getData(i);
-            seq.mapProfile(data);
+            seq.mapSequence(i, key, profileReader.getData(i));
 
-
-            writer.writeStart(thread_idx);
             if (isDbOutput == false) {
-                APPEND("Query profile of sequence ");
-                APPEND(SSTR(key));
-                APPEND("\n");
+                result.append("Query profile of sequence ");
+                Itoa::i32toa_sse2(key, buffer);
+                result.append(buffer);
+                result.push_back('\n');
             }
-            APPEND("Pos\tCns\t");
+
+            result.append("Pos\tCns");
             for (size_t aa = 0; aa < Sequence::PROFILE_AA_SIZE; aa++) {
-                APPEND(SSTR(subMat.int2aa[aa]));
-                if (aa < Sequence::PROFILE_AA_SIZE - 1) {
-                    APPEND("\t");
-                } else {
-                    APPEND("\n");
-                }
+                result.push_back('\t');
+                result.push_back(subMat.int2aa[aa]);
             }
+            result.push_back('\n');
 
-            for (int j = 0; j < seq.L; j++) {
-                APPEND(SSTR(j));
-                APPEND("\t");
-                APPEND(SSTR(subMat.int2aa[seq.int_sequence[j]]));
-                APPEND("\t");
+            for (int j = 0; j < seq.L; ++j) {
+                Itoa::i32toa_sse2(j, buffer);
+                result.append(buffer);
+                result.push_back('\t');
+                result.push_back(subMat.int2aa[seq.int_sequence[j]]);
                 for (size_t aa = 0; aa < Sequence::PROFILE_AA_SIZE; aa++) {
-                    APPEND(SSTR((int)seq.profile_for_alignment[aa * seq.L + j]));
-                    if (aa < Sequence::PROFILE_AA_SIZE - 1) {
-                        APPEND("\t");
-                    } else {
-                        APPEND("\n");
-                    }
+                    result.push_back('\t');
+                    Itoa::i32toa_sse2(seq.profile_for_alignment[aa * seq.L + j], buffer);
+                    result.append(buffer);
                 }
+                result.push_back('\n');
             }
-
-            writer.writeEnd(key, thread_idx, isDbOutput);
+            writer.writeData(result.c_str(), result.length(), key, thread_idx, isDbOutput);
         }
+        delete[] buffer;
     }
-#undef APPEND
     writer.close();
-
     if (isDbOutput == false) {
         remove(par.db2Index.c_str());
     }
 
     Debug(Debug::INFO) << "\nDone.\n";
 
-    consReader.close();
     profileReader.close();
 
     return EXIT_SUCCESS;
 }
-
