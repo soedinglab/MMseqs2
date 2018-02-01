87e10bd9636221065afca2dd745e831ea058f9b4
Milot Mirdita <milot@mirdita.de>
2018-02-01 14:24:04 +0100

    Threads and pipe+spawn were not playing well together.
    We got into race conditions where each thread
    would create new file descriptors in its process namespace
    and the spawned processes of other threads would inherit those.
    
    The spawned processes would then deadlock waiting 
    for the files to be closed in the other spawned processes.
    
    So instead we are now building our own workers
    with fork instead of threads now. Since each worker
    is a separate process now, they cannot cross-polute 
    each others file descriptor tables anymore.
    
    This is even more fun with MPI, since the forked 
    processes should never call mpi_init.

--- src/util/apply.cpp
+++ src/util/apply.cpp
@@ -4,18 +4,41 @@
 #include "Util.h"
 #include "Debug.h"
 
-#include <algorithm>
 #include <climits>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <spawn.h>
 #include <poll.h>
+#include <sys/mman.h>
+
 
 #ifdef OPENMP
 #include <omp.h>
 #endif
+
+
+int pipe2_wrap(int fd[2], int flag) {
+    int ret = pipe(fd);
+    if (ret) {
+        return ret;
+    }
+    if (flag & O_CLOEXEC) {
+        if (    fcntl(fd[0], F_SETFD, FD_CLOEXEC) == -1
+             || fcntl(fd[1], F_SETFD, FD_CLOEXEC) == -1)
+        {
+            return -1;
+        }
+    }
+    if (flag & O_NONBLOCK) {
+        if (    fcntl(fd[0], F_SETFL, O_NONBLOCK) == -1
+             || fcntl(fd[1], F_SETFL, O_NONBLOCK) == -1)
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
 
 // Analogous to gnulib implementation
 // https://www.gnu.org/software/gnulib/
@@ -25,16 +48,17 @@ pid_t create_pipe(const char *prog_path, char **prog_argv, char **environ, int f
     int ofd[2];
 
     int err;
-    if ((err = pipe(ifd)) < 0) {
+    if ((err = pipe2_wrap(ifd, O_CLOEXEC | O_NONBLOCK)) != 0) {
         perror("pipe ifd");
         errno = err;
         return -1;
     }
-    if ((err = pipe(ofd)) < 0) {
+    if ((err = pipe2_wrap(ofd, O_CLOEXEC | O_NONBLOCK)) != 0) {
         perror("pipe ofd");
         errno = err;
         return -1;
     }
+
 
     int actions_allocated = 0;
     int attrs_allocated = 0;
@@ -45,12 +69,8 @@ pid_t create_pipe(const char *prog_path, char **prog_argv, char **environ, int f
 
     if ((err = posix_spawn_file_actions_init(&actions)) != 0
         || (actions_allocated = 1,
-            (err = posix_spawn_file_actions_adddup2 (&actions, ofd[0], STDIN_FILENO)) != 0
+               (err = posix_spawn_file_actions_adddup2 (&actions, ofd[0], STDIN_FILENO))  != 0
             || (err = posix_spawn_file_actions_adddup2 (&actions, ifd[1], STDOUT_FILENO)) != 0
-            || (err = posix_spawn_file_actions_addclose(&actions, ofd[0])) != 0
-            || (err = posix_spawn_file_actions_addclose(&actions, ifd[1])) != 0
-            || (err = posix_spawn_file_actions_addclose(&actions, ofd[1])) != 0
-            || (err = posix_spawn_file_actions_addclose(&actions, ifd[0])) != 0
             #ifdef POSIX_SPAWN_USEVFORK
             || ((err = posix_spawnattr_init(&attrs)) != 0
                 || (attrs_allocated = 1,
@@ -93,7 +113,7 @@ pid_t create_pipe(const char *prog_path, char **prog_argv, char **environ, int f
 }
 
 int apply_by_entry(char* data, size_t size, unsigned int key, DBWriter& writer,
-                   const char* program_name, char ** program_argv, char **environ, unsigned int thread_idx) {
+                   const char* program_name, char ** program_argv, char **environ, unsigned int proc_idx) {
     // only works with the environ we construct ourselves
     // local_environment() leaves the first element free to use for ourselves
     snprintf(environ[0], 64, "MMSEQS_ENTRY_NAME=%d", key);
@@ -109,18 +129,9 @@ int apply_by_entry(char* data, size_t size, unsigned int key, DBWriter& writer,
     // Analogous to gnulib implementation
     size_t written = 0;
     int error = 0;
-    int fcntl_flags;
-    if ( (fcntl_flags = fcntl(fd[1], F_GETFL, 0)) < 0
-         || fcntl(fd[1], F_SETFL, fcntl_flags | O_NONBLOCK) == -1
-         || (fcntl_flags = fcntl(fd[0], F_GETFL, 0)) < 0
-         || fcntl(fd[0], F_SETFL, fcntl_flags | O_NONBLOCK) == -1)
-    {
-        goto end;
-    }
 
     char buffer[PIPE_BUF];
-    writer.writeStart(thread_idx);
-
+    writer.writeStart(proc_idx);
     struct pollfd plist[2];
     for (;;) {
         size_t rest = size - written;
@@ -179,7 +190,7 @@ int apply_by_entry(char* data, size_t size, unsigned int key, DBWriter& writer,
         } else if (plist[1].revents & POLLIN) {
             ssize_t bytes_read = read(plist[1].fd, &buffer, sizeof(buffer));
             if (bytes_read > 0) {
-                writer.writeAdd(buffer, bytes_read, thread_idx);
+                writer.writeAdd(buffer, bytes_read, proc_idx);
             } else if (bytes_read < 0) {
                 if (errno != EAGAIN) {
                     perror("read stdout0");
@@ -197,7 +208,7 @@ int apply_by_entry(char* data, size_t size, unsigned int key, DBWriter& writer,
 
     end:
 
-    writer.writeEnd(key, thread_idx, true);
+    writer.writeEnd(key, proc_idx, true);
 
     if (write_closed == true) {
         close(fd[1]);
@@ -260,16 +271,8 @@ void free_local_environment(char** local_environ) {
 }
 
 int apply(int argc, const char **argv, const Command& command) {
-    ignore_signal(SIGPIPE);
-
-    MMseqsMPI::init(argc, argv);
-
     Parameters& par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, 2, true, Parameters::PARSE_REST);
-
-#ifdef OPENMP
-    omp_set_num_threads(par.threads);
-#endif
 
     DBReader<unsigned int> reader(par.db1.c_str(), par.db1Index.c_str());
 
@@ -280,73 +283,123 @@ int apply(int argc, const char **argv, const Command& command) {
     // force the other processes / threads to stall
     reader.open(DBReader<unsigned int>::SHUFFLE);
 
+    const unsigned int *sizes = reader.getSeqLens();
+
 #ifdef HAVE_MPI
-    size_t dbFrom = 0;
-    size_t dbSize = 0;
-    Util::decomposeDomainByAminoAcid(reader.getAminoAcidDBSize(), reader.getSeqLens(), reader.getSize(),
-                                     MMseqsMPI::rank, MMseqsMPI::numProc, &dbFrom, &dbSize);
-    std::pair<std::string, std::string> tmpOutput = Util::createTmpFileNames(par.db2, par.db2Index, MMseqsMPI::rank);
-    const char* resultData = tmpOutput.first.c_str();
-    const char* resultIndex = tmpOutput.second.c_str();
-#else
-    size_t dbFrom = 0;
-    size_t dbSize = reader.getSize();
-    const char* resultData = par.db2.c_str();
-    const char* resultIndex = par.db2Index.c_str();
+    struct worker_s {
+        int ready;
+        int mpiRank;
+        int mpiProc;
+    };
+
+    worker_s* shared_memory = (worker_s*)mmap(NULL, sizeof(worker_s), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    memset(shared_memory, 0, sizeof(worker_s));
 #endif
 
-    DBWriter writer(resultData, resultIndex, par.threads);
-    writer.open();
-
-    unsigned int *sizes = reader.getSeqLens();
     Debug(Debug::INFO) << "Start applying.\n";
-#pragma omp parallel
-    {
-        char **local_environ = local_environment();
+    for (int proc_idx = 0; proc_idx < par.threads; ++proc_idx) {
+        switch(fork()) {
+            default:
+                break;
+            case -1:
+                Debug(Debug::ERROR) << "Could not fork worker process!\n";
+                EXIT(EXIT_FAILURE);
+            case 0: {
+                size_t dbFrom = 0;
+                size_t dbSize = 0;
+#ifdef HAVE_MPI
+                while (shared_memory->ready == 0) {
+                    usleep(10);
+                }
 
-        unsigned int thread_idx = 0;
-#ifdef OPENMP
-        thread_idx = static_cast<unsigned int>(omp_get_thread_num());
+                Util::decomposeDomainByAminoAcid(reader.getAminoAcidDBSize(), reader.getSeqLens(), reader.getSize(),
+                                                 shared_memory->mpiRank, shared_memory->mpiProc, &dbFrom, &dbSize);
+
+                std::pair<std::string, std::string> mpiDb = Util::createTmpFileNames(par.db2, par.db2Index, shared_memory->mpiRank);
+                Util::decomposeDomain(dbSize, proc_idx, par.threads, &dbFrom, &dbSize);
+                std::pair<std::string, std::string> outDb = Util::createTmpFileNames(mpiDb.first, mpiDb.second, proc_idx);
+#else
+                Util::decomposeDomainByAminoAcid(reader.getAminoAcidDBSize(), reader.getSeqLens(), reader.getSize(),
+                                                 proc_idx, par.threads, &dbFrom, &dbSize);
+
+                std::pair<std::string, std::string> outDb = Util::createTmpFileNames(par.db2, par.db2Index, proc_idx);
 #endif
 
-        std::pair<unsigned int, unsigned int> *lengthSorted = new std::pair<unsigned int, unsigned int>[dbSize];
-        for (size_t i = dbFrom; i < (dbFrom + dbSize); i++) {
-            lengthSorted[i - dbFrom] = std::make_pair(i, sizes[i]);
-        }
-        std::stable_sort(lengthSorted, lengthSorted + dbSize, DBReader<unsigned int>::comparePairBySeqLength());
+                DBWriter writer(outDb.first.c_str(), outDb.second.c_str(), 1);
+                writer.open();
 
-#pragma omp for schedule(dynamic, 1)
-        for (size_t i = 0; i < dbSize; ++i) {
-            Debug::printProgress(i);
+                char **local_environ = local_environment();
 
-            size_t index = lengthSorted[i].first;
-            size_t size = lengthSorted[i].second - 1;
+                std::pair<unsigned int, unsigned int> *lengthSorted = new std::pair<unsigned int, unsigned int>[dbSize];
+                for (size_t i = dbFrom; i < (dbFrom + dbSize); i++) {
+                    lengthSorted[i - dbFrom] = std::make_pair(i, sizes[i]);
+                }
+                std::stable_sort(lengthSorted, lengthSorted + dbSize, DBReader<unsigned int>::comparePairBySeqLength());
 
-            char *data = reader.getData(index);
-            if (data == NULL) {
-                continue;
+                ignore_signal(SIGPIPE);
+
+                for (size_t i = 0; i < dbSize; ++i) {
+                    Debug::printProgress(i);
+
+                    size_t index = lengthSorted[i].first;
+                    size_t size = lengthSorted[i].second - 1;
+
+                    char *data = reader.getData(index);
+                    if (data == NULL) {
+                        continue;
+                    }
+
+                    unsigned int key = reader.getDbKey(index);
+                    int status = apply_by_entry(data, size, key, writer, par.restArgv[0], const_cast<char**>(par.restArgv), local_environ, 0);
+                    if (status == -1) {
+                        Debug(Debug::WARNING) << "Entry " << index << " system error " << errno << "!\n";
+                        continue;
+                    } else if (status > 0) {
+                        Debug(Debug::WARNING) << "Entry " << index << " exited with error code " << status << "!\n";
+                        continue;
+                    }
+                }
+
+                delete[] lengthSorted;
+
+                writer.close();
+                reader.close();
+                free_local_environment(local_environ);
+                _Exit(0);
             }
-
-            unsigned int key = reader.getDbKey(index);
-            int status = apply_by_entry(data, size, key, writer, par.restArgv[0], const_cast<char**>(par.restArgv), local_environ, thread_idx);
-            if (status == -1) {
-                Debug(Debug::WARNING) << "Entry " << index << " system error " << errno << "!\n";
-                continue;
-            } else if (status > 0) {
-                Debug(Debug::WARNING) << "Entry " << index << " exited with error code " << status << "!\n";
-                continue;
-            }
         }
-
-        delete[] lengthSorted;
-
-        free_local_environment(local_environ);
     }
-    Debug(Debug::INFO) << "\n";
-    writer.close();
 
+    MMseqsMPI::init(argc, argv);
+#ifdef HAVE_MPI
+    shared_memory->mpiRank = MMseqsMPI::rank;
+    shared_memory->mpiProc = MMseqsMPI::numProc;
+    __sync_fetch_and_add(&(shared_memory->ready), 1);
+#endif
+
+    for (int proc_idx = 0; proc_idx < par.threads; ++proc_idx) {
+        int status = 0;
+        while (waitpid(-1, &status, 0) == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+        }
+    }
     Debug(Debug::INFO) << "\nDone.\n";
+
     reader.close();
+
+#ifdef HAVE_MPI
+    std::pair<std::string, std::string> outDb = Util::createTmpFileNames(par.db2, par.db2Index, MMseqsMPI::rank);
+#else
+    std::pair<std::string, std::string> outDb = std::make_pair(par.db2, par.db2Index);
+#endif
+
+    std::vector<std::pair<std::string, std::string>> splitFiles;
+    for (int proc_idx = 0; proc_idx < par.threads; ++proc_idx) {
+        splitFiles.emplace_back(Util::createTmpFileNames(outDb.first, outDb.second, proc_idx));
+    }
+    DBWriter::mergeResults(outDb.first, outDb.second, splitFiles);
 
 #ifdef HAVE_MPI
     MPI_Barrier(MPI_COMM_WORLD);
