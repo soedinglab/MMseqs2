31e51b41586881e6641fa769fab45dbb3226aa69
Unknown <clovis.norroy@gmail.com>
2018-04-27 13:40:52 +0200

    Revert "Added new modes for filterdb"
    This reverts commit c7d9130d99736de8fcc7cfb255e32d078321b09d.

--- src/commons/Parameters.h
+++ src/commons/Parameters.h
@@ -317,8 +317,6 @@ public:
     int sortEntries;
     bool beatsFirst;
     std::string joinDB;
-    std::string swapFields ;
-    std::string clusterFile ;
 
     //aggregate
     std::string mode ;
@@ -554,8 +552,6 @@ public:
     PARAMETER(PARAM_SORT_ENTRIES)
     PARAMETER(PARAM_BEATS_FIRST)
     PARAMETER(PARAM_JOIN_DB)
-    PARAMETER(PARAM_SWAP_SEARCH_FIELDS)
-    PARAMETER(PARAM_TRANSITIVE_REPLACE)
 
     //aggregate
     PARAMETER(PARAM_MODE)
