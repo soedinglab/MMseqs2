6daf27f36058348241e0174e96b27b6f4faf9587
Milot Mirdita <milot@mirdita.de>
2018-06-26 10:48:24 +0200

    whitespace

--- src/util/msa2profile.cpp
+++ src/util/msa2profile.cpp
@@ -181,15 +181,16 @@ int msa2profile(int argc, const char **argv, const Command &command) {
                 d.length = entryLength;
             }
             d.position = 0;
-            // remove the comment line that can make kseq_read failing
-            if (d.length)
-            {
-                if (d.buffer[0]=='#')
-                {
-		    size_t pos = 0;
-                    while(pos<d.length && d.buffer[pos] != '\n')pos++;
-                    if (pos<d.length)
-                    {
+
+            // remove comment line that makes kseq_read fail
+            if (d.length) {
+                if (d.buffer[0] == '#') {
+		            size_t pos = 0;
+                    while (pos < d.length && d.buffer[pos] != '\n') {
+                        pos++;
+                    }
+
+                    if (pos < d.length) {
                         pos++;
                         d.buffer += pos;
                         d.length -= pos;
@@ -199,12 +200,12 @@ int msa2profile(int argc, const char **argv, const Command &command) {
                     }
                 }
             }
+
             while (kseq_read(seq) >= 0) {
                 if (seq->name.l == 0 || seq->seq.l == 0) {
                     Debug(Debug::WARNING) << "Invalid fasta sequence "
                                           << setSize << " in entry " << queryKey << "!\n";
                     fastaError = true;
-		   
                     break;
                 }
 
