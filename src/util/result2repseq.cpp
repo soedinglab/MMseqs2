192ba52f9f466956c2d4a04030641c91972c8a00
Milot Mirdita <milot@mirdita.de>
2018-08-29 16:26:03 +0200

    Cleanup result2repseq

--- src/util/result2repseq.cpp
+++ src/util/result2repseq.cpp
@@ -1,6 +1,3 @@
-#include <string>
-
-#include "Sequence.h"
 #include "Parameters.h"
 #include "DBReader.h"
 #include "DBWriter.h"
@@ -11,62 +8,47 @@
 #include <omp.h>
 #endif
 
-int result2repseq(const Parameters &par, DBReader<unsigned int> &resultReader,
-                   const std::string &outDb, const size_t dbFrom, const size_t dbSize) {
-
-    DBReader<unsigned int> qDbr(par.db1.c_str(), par.db1Index.c_str());
-    qDbr.open(DBReader<unsigned int>::NOSORT);
-
-    std::string outIndex(outDb);
-    outIndex.append(".index");
-
-    DBWriter resultWriter(outDb.c_str(), outIndex.c_str(), par.threads);
-    resultWriter.open();
-
-    Debug(Debug::INFO) << "Start computing representative sequences.\n";
-#pragma omp parallel for schedule(dynamic, 100)
-    for (size_t id = dbFrom; id < (dbFrom + dbSize); id++) {
-        Debug::printProgress(id);
-        unsigned int thread_idx = 0;
-#ifdef OPENMP
-        thread_idx = (unsigned int) omp_get_thread_num();
-#endif
-        char *results = resultReader.getData(id);
-        if (*results == '\0') {
-            Debug(Debug::WARNING) << "Empty result for entry " << id << "!\n";
-            continue;
-        }
-
-        char dbKey[255];
-        Util::parseKey(results, dbKey);
-        const unsigned int key = (unsigned int) strtoul(dbKey, NULL, 10);
-        const size_t edgeId = qDbr.getId(key);
-
-        std::string result(qDbr.getData(edgeId));
-        unsigned int queryKey = resultReader.getDbKey(id);
-        resultWriter.writeData(result.c_str(), result.length(), queryKey, thread_idx);
-    }
-
-    resultWriter.close(Sequence::AMINO_ACIDS);
-    qDbr.close();
-
-    Debug(Debug::INFO) << "\nDone.\n";
-    return EXIT_SUCCESS;
-}
-
 int result2repseq(int argc, const char **argv, const Command &command) {
     Parameters &par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, 3);
 
+    DBReader<unsigned int> seqReader(par.db1.c_str(), par.db1Index.c_str());
+    seqReader.open(DBReader<unsigned int>::NOSORT);
+
     DBReader<unsigned int> resultReader(par.db2.c_str(), par.db2Index.c_str());
     resultReader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
 
-    int status;
+    DBWriter resultWriter(par.db3.c_str(), par.db3Index.c_str(), par.threads);
+    resultWriter.open();
 
-    size_t resultSize = resultReader.getSize();
-    status = result2repseq(par, resultReader, par.db3, 0, resultSize);
+    Debug(Debug::INFO) << "Start computing representative sequences.\n";
+#pragma omp parallel
+    {
+        unsigned int thread_idx = 0;
+#ifdef OPENMP
+        thread_idx = (unsigned int) omp_get_thread_num();
+#endif
 
+        char dbKey[255];
+#pragma omp parallel for schedule(dynamic, 100)
+        for (size_t id = 0; id < resultReader.getSize(); ++id) {
+            Debug::printProgress(id);
+
+            char *results = resultReader.getData(id);
+            if (*results == '\0') {
+                continue;
+            }
+
+            Util::parseKey(results, dbKey);
+            const unsigned int key = (unsigned int) strtoul(dbKey, NULL, 10);
+            const size_t edgeId = seqReader.getId(key);
+            resultWriter.writeData(seqReader.getData(edgeId), seqReader.getSeqLens(edgeId) - 1, resultReader.getDbKey(id), thread_idx);
+        }
+    }
+    Debug(Debug::INFO) << "\n";
+    resultWriter.close(seqReader.getDbtype());
     resultReader.close();
+    seqReader.close();
 
-    return status;
+    return EXIT_SUCCESS;
 }
