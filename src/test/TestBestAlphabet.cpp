a72d3b8b2a3be05bbfab40741a3099ba5c0a32df
Martin Steinegger <themartinsteinegger@gmail.com>
2018-12-11 18:06:34 -0500

    Add TestBestAlphabet

---
+++ src/test/TestBestAlphabet.cpp
@@ -0,0 +1,41 @@
+//  main.cpp
+//  forautocompl
+//
+//  Created by Martin Steinegger on 26.11.12.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+#include <iostream>
+#include <cmath>
+
+#include "StripedSmithWaterman.h"
+#include "Sequence.h"
+#include "Indexer.h"
+#include "ExtendedSubstitutionMatrix.h"
+#include "SubstitutionMatrix.h"
+#include "ReducedMatrix.h"
+#include "KmerGenerator.h"
+#include "BaseMatrix.h"
+#include "StripedSmithWaterman.h"
+#include "Parameters.h"
+#include "Matcher.h"
+4:55 PM
+const char* binary_name = "test_bestalphabet";
+
+int main (int, const char**) {
+    SubstitutionMatrix subMat("blosum62.out", 2.0, -0.0f);
+    std::cout << "Subustitution matrix:\n";
+    SubstitutionMatrix::print(subMat.subMatrix,subMat.int2aa,subMat.alphabetSize);
+    for(size_t aa1 = 0; aa1 < subMat.alphabetSize; aa1++) {
+        for (size_t aa2 = 0; aa2 < subMat.alphabetSize; aa2++) {
+            printf("%.4f\t", subMat.probMatrix[aa1][aa2]);
+        }
+        printf("\n");
+    }
+
+    for(size_t aa1 = 0; aa1 < subMat.alphabetSize; aa1++) {
+        printf("%.5f\t", subMat.pBack[aa1]);
+    }
+    printf("\n");
+
+    return 0;
+}
