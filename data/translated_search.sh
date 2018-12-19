fb102af4055c8aee0bc68d3ce25805d2675812e1
Milot Mirdita <milot@mirdita.de>
2018-12-19 17:06:43 +0100

    Fix mapping workflow #144

--- data/translated_search.sh
+++ data/translated_search.sh
@@ -56,7 +56,7 @@ fi
 mkdir -p "$4/search"
 if notExists "$4/aln"; then
     # shellcheck disable=SC2086
-    "$SEARCH" "${QUERY}" "${TARGET}" "$4/aln" "$4/search" ${SEARCH_PAR} \
+    "$SEARCH" "${QUERY}" "${TARGET}" "$4/aln" "$4/search" \
         || fail "Search step died"
 fi
 
