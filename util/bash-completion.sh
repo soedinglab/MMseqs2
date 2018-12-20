95534e1bd7186dce99feed238e16a8501d4db9eb
Milot Mirdita <milot@mirdita.de>
2018-12-20 14:23:07 +0100

    Silence mmseqs not found errors in shell completion, if mmseqs is not in PATH

--- util/bash-completion.sh
+++ util/bash-completion.sh
@@ -5,12 +5,12 @@ _mmseqs() {
 	cur="${COMP_WORDS[COMP_CWORD]}"
 
 	if [[ ${COMP_CWORD} -eq 1 ]] ; then
-		COMPREPLY=( $(LC_COLLATE=C compgen -W "$(mmseqs shellcompletion)" -- "${cur}") )
+		COMPREPLY=( $(LC_COLLATE=C compgen -W "$(mmseqs shellcompletion 2> /dev/null)" -- "${cur}") )
 		return 0
 	fi
 
 	if [[ ${COMP_CWORD} -gt 1 ]] ; then
-		COMPREPLY=( $(LC_COLLATE=C compgen -f -W "$(mmseqs shellcompletion "${COMP_WORDS[1]}")" -- "${cur}") )
+		COMPREPLY=( $(LC_COLLATE=C compgen -f -W "$(mmseqs shellcompletion "${COMP_WORDS[1]}" 2> /dev/null)" -- "${cur}") )
 		return 0
 	fi
 
