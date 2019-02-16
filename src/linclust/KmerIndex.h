34372fa9856acb4a1acc25b8180b8a130ca183ca
Martin Steinegger <themartinsteinegger@gmail.com>
2019-02-15 19:09:11 -0500

    Remove madvice for now, todo add it later

--- src/linclust/KmerIndex.h
+++ src/linclust/KmerIndex.h
@@ -183,11 +183,11 @@ public:
         this->entryCount = entryCount;
         this->indexGridSize = MathUtil::ceilIntDivision( MathUtil::ipow<size_t>(alphabetSize, kmerSize), gridResolution );
         this->entryOffsets = (size_t *) entriesOffetData;
-#if HAVE_POSIX_FADVISE
-        if (posix_madvise (entriesData, entriesOffetData, POSIX_FADV_SEQUENTIAL) != 0){
-            Debug(Debug::ERROR) << "KmerIndex posix_madvise returned an error\n";
-        }
-#endif
+//#if HAVE_POSIX_FADVISE
+//        if (posix_madvise (entriesData, entriesOffetData, POSIX_FADV_SEQUENTIAL) != 0){
+//            Debug(Debug::ERROR) << "KmerIndex posix_madvise returned an error\n";
+//        }
+//#endif
 
         this->prevKmerStartRange = 0;
         this->iteratorPos = -1;
