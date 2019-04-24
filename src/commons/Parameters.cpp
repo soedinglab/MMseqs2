c32460eb0bc5317890baae007da8c28e452bf2e1
Martin Steinegger <themartinsteinegger@gmail.com>
2019-04-24 03:49:31 -0400

    Turn adjust mode off in default

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -1703,7 +1703,7 @@ void Parameters::setDefaults() {
     skipNRepeatKmer = 0;
     hashShift = 5;
     pickNbest = 1;
-    adjustKmerLength = true;
+    adjustKmerLength = false;
     // result2stats
     stat = "";
 
