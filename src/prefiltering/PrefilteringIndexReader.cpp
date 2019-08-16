3b08698ddc2897e9bd36147803f300e673c9dd72
Milot Mirdita <milot@mirdita.de>
2019-08-16 18:03:49 +0200

    Fix wrong range of split precomputed index

--- src/prefiltering/PrefilteringIndexReader.cpp
+++ src/prefiltering/PrefilteringIndexReader.cpp
@@ -201,7 +201,7 @@ void PrefilteringIndexReader::createIndexFile(const std::string &outDB,
         IndexBuilder::fillDatabase(&indexTable,
                                    (maskMode == 1 || maskLowerCase == 1) ? &sequenceLookup : NULL,
                                    (maskMode == 0 ) ? &sequenceLookup : NULL,
-                                   *subMat, &seq, dbr1, dbFrom, dbSize, kmerThr, maskMode, maskLowerCase);
+                                   *subMat, &seq, dbr1, dbFrom, dbFrom + dbSize, kmerThr, maskMode, maskLowerCase);
         indexTable.printStatistics(subMat->int2aa);
 
         if (sequenceLookup == NULL) {
