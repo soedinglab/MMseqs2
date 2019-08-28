e4487fa66deaedbde8619c25d50333ad184c76e5
Martin Steinegger <themartinsteinegger@gmail.com>
2019-08-28 16:14:16 -0400

    Fix compiler error in test

--- src/test/TestDBReaderIndexSerialization.cpp
+++ src/test/TestDBReaderIndexSerialization.cpp
@@ -8,14 +8,14 @@ int main (int, const char**) {
     reader.open(DBReader<unsigned int>::NOSORT);
 
     Debug(Debug::INFO) << reader.getSize() << " " << reader.getAminoAcidDBSize() << "\n";
-    Debug(Debug::INFO) << reader.getIndex()[0].id  << " " << reader.getIndex()[0].offset  << " " << reader.getSeqLens()[0] << "\n";
+    Debug(Debug::INFO) << reader.getIndex()[0].id  << " " << reader.getIndex()[0].offset  << " " << reader.getIndex()[0].length  << "\n";
 
     char* data = DBReader<unsigned int>::serialize(reader);
     DBReader<unsigned int>* newdbr = DBReader<unsigned int>::unserialize(data, 1);
     newdbr->open(DBReader<unsigned int>::NOSORT);
 
     Debug(Debug::INFO) << newdbr->getSize() << " " << newdbr->getAminoAcidDBSize() << "\n";
-    Debug(Debug::INFO) << newdbr->getIndex()[0].id  << " " << newdbr->getIndex()[0].offset  << " " << newdbr->getSeqLens()[0] << "\n";
+    Debug(Debug::INFO) << newdbr->getIndex()[0].id  << " " << newdbr->getIndex()[0].offset  << " " << newdbr->getIndex()[0].length << "\n";
     free(data);
 
     newdbr->close();
