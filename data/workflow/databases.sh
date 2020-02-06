c0c538ea3d7c12f5231bf75127c3cd1823645084
Milot Mirdita <milot@mirdita.de>
2020-02-06 14:59:32 +0100

    Various fixes in databases script

--- data/workflow/databases.sh
+++ data/workflow/databases.sh
@@ -12,6 +12,17 @@ hasCommand () {
     command -v "$1" >/dev/null 2>&1
 }
 
+ARR=""
+push_back() {
+    # shellcheck disable=SC1003
+    CURR="$(printf '%s' "$1" | awk '{ gsub(/'\''/, "'\''\\'\'''\''"); print; }')"
+    if [ -z "$ARR" ]; then
+        ARR=''\'$CURR\'''
+    else
+        ARR=$ARR' '\'$CURR\'''
+    fi
+}
+
 STRATEGY=""
 if hasCommand aria2c; then STRATEGY="$STRATEGY ARIA"; fi
 if hasCommand curl;   then STRATEGY="$STRATEGY CURL"; fi
@@ -52,110 +63,120 @@ TMP_PATH="$3"
 INPUT_TYPE=""
 case "${SELECTION}" in
     "UniRef100")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.release_note" "${OUTDB}.version"
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.fasta.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/uniref100.fasta.gz"; then
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.release_note" "${TMP_PATH}/version"
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.fasta.gz" "${TMP_PATH}/uniref100.fasta.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/uniref100.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "UniRef90")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.release_note" "${OUTDB}.version"
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/uniref90.fasta.gz"; then
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.release_note" "${TMP_PATH}/version"
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz" "${TMP_PATH}/uniref90.fasta.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/uniref90.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "UniRef50")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.release_note" "${OUTDB}.version"
-          downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.fasta.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/uniref50.fasta.gz"; then
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.release_note" "${TMP_PATH}/version"
+            downloadFile "ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.fasta.gz" "${TMP_PATH}/uniref50.fasta.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/uniref50.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "UniProtKB")
-        if notExists "${TMP_PATH}/db1.fasta.gz" || notExists "${TMP_PATH}/db2.fasta.gz"; then
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${OUTDB}.version"
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz" "${TMP_PATH}/db1.fasta.gz"
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz" "${TMP_PATH}/db2.fasta.gz"
+        if notExists "${TMP_PATH}/uniprot_sprot.fasta.gz" || notExists "${TMP_PATH}/uniprot_trembl.fasta.gz"; then
+            downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${TMP_PATH}/version"
+            downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz" "${TMP_PATH}/uniprot_sprot.fasta.gz"
+            downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz" "${TMP_PATH}/uniprot_trembl.fasta.gz"
         fi
-        INPUT_TYPE="AA_2"
+        push_back "${TMP_PATH}/uniprot_sprot.fasta.gz"
+        push_back "${TMP_PATH}/uniprot_trembl.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "UniProtKB/TrEMBL")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${OUTDB}.version"
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/uniprot_trembl.fasta.gz"; then
+          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${TMP_PATH}/version"
+          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz" "${TMP_PATH}/uniprot_trembl.fasta.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/uniprot_trembl.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "UniProtKB/Swiss-Prot")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${OUTDB}.version"
-          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/uniprot_sprot.fasta.gz"; then
+          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt" "${TMP_PATH}/version"
+          downloadFile "https://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz" "${TMP_PATH}/uniprot_sprot.fasta.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/uniprot_sprot.fasta.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "NR")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          date "+%s" > "${OUTDB}.version"
-          downloadFile "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/nr.gz"; then
+            date "+%s" > "${TMP_PATH}/version"
+            downloadFile "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz" "${TMP_PATH}/nr.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/nr.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "NT")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          date "+%s" > "${OUTDB}.version"
-          downloadFile "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/nt.gz"; then
+            date "+%s" > "${TMP_PATH}/version"
+            downloadFile "https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz" "${TMP_PATH}/nt.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/nt.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "PDB")
-        if notExists "${TMP_PATH}/db.fasta.gz"; then
-          date "+%s" > "${OUTDB}.version"
-          downloadFile "https://ftp.wwpdb.org/pub/pdb/derived_data/pdb_seqres.txt.gz" "${TMP_PATH}/db.fasta.gz"
+        if notExists "${TMP_PATH}/pdb_seqres.txt.gz"; then
+            date "+%s" > "${TMP_PATH}/version"
+            downloadFile "https://ftp.wwpdb.org/pub/pdb/derived_data/pdb_seqres.txt.gz" "${TMP_PATH}/pdb_seqres.txt.gz"
         fi
-        INPUT_TYPE="AA"
+        push_back "${TMP_PATH}/pdb_seqres.txt.gz"
+        INPUT_TYPE="FASTA_LIST"
     ;;
     "PDB70")
         if notExists "${TMP_PATH}/msa.index"; then
-          date "+%s" > "${OUTDB}.version"
-          downloadFile "http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_latest.tar.gz" "${TMP_PATH}/pdb70.tar.gz"
-          tar -xOzf "${TMP_PATH}/pdb70.tar.gz" pdb70_a3m.ffdata | tr -d '\000' | awk -v outfile="${TMP_PATH}/msa" 'function writeEntry() { printf "%s\0", data >> outfile; size = length(data) + 1; data=""; print id"\t"offset"\t"size >> outindex; offset = offset + size; } BEGIN { data = ""; offset = 0; id = 1; if(length(outfile) == 0) { outfile="output"; } outindex = outfile".index"; printf("") > outfile; printf("") > outindex; } /^>ss_/ { inss = 1; entry = 0; next; } inss == 1 { inss = 0; next; } /^>/ && entry == 0 { if (id > 1) { writeEntry(); } id = id + 1; data = ">"substr($1, 2)"\n"; entry = entry + 1; next; } entry > 0 { data = data""$0"\n"; entry = entry + 1; next; } END { writeEntry(); close(outfile); close(outfile".index"); }'
-          rm -f "${TMP_PATH}/pdb70.tar.gz"
+            date "+%s" > "${TMP_PATH}/version"
+            downloadFile "http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_latest.tar.gz" "${TMP_PATH}/pdb70.tar.gz"
+            tar -xOzf "${TMP_PATH}/pdb70.tar.gz" pdb70_a3m.ffdata | tr -d '\000' | awk -v outfile="${TMP_PATH}/msa" 'function writeEntry() { printf "%s\0", data >> outfile; size = length(data) + 1; data=""; print id"\t"offset"\t"size >> outindex; offset = offset + size; } BEGIN { data = ""; offset = 0; id = 1; if(length(outfile) == 0) { outfile="output"; } outindex = outfile".index"; printf("") > outfile; printf("") > outindex; } /^>ss_/ { inss = 1; entry = 0; next; } inss == 1 { inss = 0; next; } /^>/ && entry == 0 { if (id > 1) { writeEntry(); } id = id + 1; data = ">"substr($1, 2)"\n"; entry = entry + 1; next; } entry > 0 { data = data""$0"\n"; entry = entry + 1; next; } END { writeEntry(); close(outfile); close(outfile".index"); }'
+            rm -f "${TMP_PATH}/pdb70.tar.gz"
         fi
         INPUT_TYPE="A3M"
     ;;
     "Pfam-A.full")
         if notExists "${TMP_PATH}/db.msa.gz"; then
-          downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam.version.gz" "${OUTDB}.version"
-          downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.full.gz" "${TMP_PATH}/db.msa.gz"
+            downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam.version.gz" "${TMP_PATH}/version"
+            downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.full.gz" "${TMP_PATH}/db.msa.gz"
         fi
         INPUT_TYPE="MSA"
     ;;
     "Pfam-A.seed")
         if notExists "${TMP_PATH}/db.msa.gz"; then
-          downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam.version.gz" "${OUTDB}.version"
-          downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz" "${TMP_PATH}/db.msa.gz"
+            downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam.version.gz" "${TMP_PATH}/version"
+            downloadFile "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz" "${TMP_PATH}/db.msa.gz"
         fi
         INPUT_TYPE="MSA"
     ;;
     "eggNOG")
         if notExists "${TMP_PATH}/download.done"; then
-          date "+%s" > "${OUTDB}.version"
-          downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2/2_raw_algs.tar" "${TMP_PATH}/bacteria"
-          downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2157/2157_raw_algs.tar" "${TMP_PATH}/archea"
-          downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2759/2759_raw_algs.tar" "${TMP_PATH}/eukaryota"
-          downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/10239/10239_raw_algs.tar" "${TMP_PATH}/viruses"
-          touch "${TMP_PATH}/download.done"
+            date "+%s" > "${TMP_PATH}/version"
+            downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2/2_raw_algs.tar" "${TMP_PATH}/bacteria"
+            downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2157/2157_raw_algs.tar" "${TMP_PATH}/archea"
+            downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/2759/2759_raw_algs.tar" "${TMP_PATH}/eukaryota"
+            downloadFile "http://eggnogdb.embl.de/download/eggnog_5.0/per_tax_level/10239/10239_raw_algs.tar" "${TMP_PATH}/viruses"
+            touch "${TMP_PATH}/download.done"
         fi
         INPUT_TYPE="eggNOG"
     ;;
     "Resfinder")
         if notExists "${TMP_PATH}/download.done"; then
-          downloadFile "https://api.bitbucket.org/2.0/repositories/genomicepidemiology/resfinder_db/commit/master?fields=hash,date" "${OUTDB}.version"
-          downloadFile "https://bitbucket.org/genomicepidemiology/resfinder_db/get/master.tar.gz" "${TMP_PATH}/master.tar.gz"
-          tar -C "${TMP_PATH}" --strip-components=1 -xzvf "${TMP_PATH}/master.tar.gz" "*.fsa"
-          rm -f "${TMP_PATH}/master.tar.gz"
-          touch "${TMP_PATH}/download.done"
+            downloadFile "https://api.bitbucket.org/2.0/repositories/genomicepidemiology/resfinder_db/commit/master?fields=hash,date" "${TMP_PATH}/version"
+            downloadFile "https://bitbucket.org/genomicepidemiology/resfinder_db/get/master.tar.gz" "${TMP_PATH}/master.tar.gz"
+            tar -C "${TMP_PATH}" --strip-components=1 -xzvf "${TMP_PATH}/master.tar.gz" "*.fsa"
+            rm -f "${TMP_PATH}/master.tar.gz"
+            touch "${TMP_PATH}/download.done"
         fi
         INPUT_TYPE="FSA"
     ;;
@@ -163,17 +184,14 @@ esac
 
 if notExists "${OUTDB}.dbtype"; then
 case "${INPUT_TYPE}" in
-    "AA")
+    "FASTA_LIST")
+        eval "set -- $ARR"
         # shellcheck disable=SC2086
-        "${MMSEQS}" createdb "${TMP_PATH}/db.fasta.gz" "${OUTDB}" ${COMP_PAR} \
+        "${MMSEQS}" createdb "${@}" "${OUTDB}" ${COMP_PAR} \
             || fail "createdb died"
-        rm -f "${TMP_PATH}/db.fasta.gz"
-    ;;
-    "AA_2")
-        # shellcheck disable=SC2086
-        "${MMSEQS}" createdb "${TMP_PATH}/db1.fasta.gz" "${TMP_PATH}/db2.fasta.gz" "${OUTDB}" ${COMP_PAR} \
-            || fail "createdb died"
-        rm -f "${TMP_PATH}/db1.fasta.gz" "${TMP_PATH}/db2.fasta.gz"
+        for i in "${@}"; do
+            rm -f -- "$i"
+        done
     ;;
     "FSA")
         # shellcheck disable=SC2086
@@ -219,12 +237,16 @@ if [ -n "${TAXONOMY}" ] && notExists "${OUTDB}_mapping"; then
     # shellcheck disable=SC2086
     "${MMSEQS}" prefixid "${OUTDB}_h" "${TMP_PATH}/header_pref.tsv" --tsv ${THREADS_PAR} \
         || fail "prefixid died"
-    awk '{match($0, /(OX|TaxID)=[0-9]+ /); print $1"\t"substr($0, RSTART+3, RLENGTH-4); }' "${TMP_PATH}/header_pref.tsv" \
+    awk '{ match($0, / OX=[0-9]+ /); if (RLENGTH != -1) { print $1"\t"substr($0, RSTART+4, RLENGTH-5); next; } match($0, / TaxID=[0-9]+ /); print $1"\t"substr($0, RSTART+7, RLENGTH-8); }' "${TMP_PATH}/header_pref.tsv" \
         | LC_ALL=C sort -n > "${OUTDB}_mapping"
     rm -f "${TMP_PATH}/header_pref.tsv"
     # shellcheck disable=SC2086
     "${MMSEQS}" createtaxdb "${OUTDB}" "${TMP_PATH}/taxonomy" ${THREADS_PAR} \
         || fail "createtaxdb died"
+fi
+
+if notExists "${OUTDB}.version"; then
+    mv -f "${TMP_PATH}/version" "${OUTDB}.version"
 fi
 
 if [ -n "${REMOVE_TMP}" ]; then
