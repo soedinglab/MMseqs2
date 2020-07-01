5d10ce00dc1c870995e01ec9adb0639c1d59c9ef
Milot Mirdita <milot@mirdita.de>
2020-07-01 15:04:54 +0200

    Rewrite expandaln module

--- src/util/expandaln.cpp
+++ src/util/expandaln.cpp
@@ -17,7 +17,7 @@
 #include <omp.h>
 #endif
 
-void rescoreResultByBacktrace(Matcher::result_t &result, Sequence &qSeq, Sequence &tSeq, SubstitutionMatrix &subMat, float *compositionBias, EvalueComputation &evaluer, int gapOpen, int gapExtend, int seqIdMode) {
+void rescoreResultByBacktrace(Matcher::result_t &result, Sequence &qSeq, Sequence &tSeq, SubstitutionMatrix &subMat, float *compositionBias, int gapOpen, int gapExtend) {
     size_t qPos = result.qStartPos;
     size_t tPos = result.dbStartPos;
     int score = 0;
@@ -37,11 +37,11 @@ void rescoreResultByBacktrace(Matcher::result_t &result, Sequence &qSeq, Sequenc
         char state = result.backtrace[i];
         if (state == 'M') {
             if (isTargetProf) {
-                score += tSeq.profile_for_alignment[qSeq.numSequence[qPos] * tSeq.L + tPos]  + static_cast<short>((compositionBias[i] < 0.0)? compositionBias[i] - 0.5: compositionBias[i] + 0.5);;
+                score += tSeq.profile_for_alignment[qSeq.numSequence[qPos] * tSeq.L + tPos]  + static_cast<short>((compositionBias[i] < 0.0) ? (compositionBias[i] - 0.5) : (compositionBias[i] + 0.5));
             } else if (isQueryProf) {
                 score += qSeq.profile_for_alignment[tSeq.numSequence[tPos] * qSeq.L + qPos];
             } else {
-                score += subMat.subMatrix[qSeq.numSequence[qPos]][tSeq.numSequence[tPos]] + static_cast<short>((compositionBias[i] < 0.0)? compositionBias[i] - 0.5: compositionBias[i] + 0.5);
+                score += subMat.subMatrix[qSeq.numSequence[qPos]][tSeq.numSequence[tPos]] + static_cast<short>((compositionBias[i] < 0.0) ? (compositionBias[i] - 0.5) : (compositionBias[i] + 0.5));
             }
             identities += qSeq.numSequence[qPos] == tSeq.numSequence[tPos] ? 1 : 0;
             qPos++;
@@ -64,25 +64,21 @@ void rescoreResultByBacktrace(Matcher::result_t &result, Sequence &qSeq, Sequenc
         }
         lastState = state;
     }
-    result.eval = evaluer.computeEvalue(score, qSeq.L);
-    result.score = static_cast<int>(evaluer.computeBitScore(score)+0.5);
-    result.seqId = Util::computeSeqId(seqIdMode, identities, qSeq.L, tSeq.L, result.backtrace.size());
+    result.score = score;
+//    result.eval = evaluer.computeEvalue(score, qSeq.L);
+//    result.score = static_cast<int>(evaluer.computeBitScore(score)+0.5);
+//    result.seqId = Util::computeSeqId(seqIdMode, identities, qSeq.L, tSeq.L, result.backtrace.size());
 }
 
-static bool compareHitsByKeyEvalScore(const Matcher::result_t &first, const Matcher::result_t &second) {
+static bool compareHitsByKeyScore(const Matcher::result_t &first, const Matcher::result_t &second) {
     if (first.dbKey < second.dbKey)
         return true;
     if (second.dbKey < first.dbKey)
-        return false;
-    if (first.eval < second.eval)
-        return true;
-    if (second.eval < first.eval)
         return false;
     if (first.score > second.score)
         return true;
     if (second.score > first.score)
         return false;
-
     return false;
 }
 
@@ -90,184 +86,178 @@ int expandaln(int argc, const char **argv, const Command& command) {
     Parameters &par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, true, 0, 0);
 
-    DBReader<unsigned int> queryReader(par.db1.c_str(), par.db1Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-    queryReader.open(DBReader<unsigned int>::NOSORT);
-    const int queryDbType = queryReader.getDbtype();
+    DBReader<unsigned int> aReader(par.db1.c_str(), par.db1Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+    aReader.open(DBReader<unsigned int>::NOSORT);
+    const int aSeqDbType = aReader.getDbtype();
     if (par.preloadMode != Parameters::PRELOAD_MODE_MMAP) {
-        queryReader.readMmapedDataInMemory();
+        aReader.readMmapedDataInMemory();
     }
 
-    DBReader<unsigned int> targetReader(par.db2.c_str(), par.db2Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-    targetReader.open(DBReader<unsigned int>::NOSORT);
-    const int targetDbType = targetReader.getDbtype();
-    if (par.preloadMode != Parameters::PRELOAD_MODE_MMAP) {
-        targetReader.readMmapedDataInMemory();
+    bool isCa3m = false;
+    DBReader<unsigned int> *resultAbReader = NULL;
+    DBReader<unsigned int> *cReader = NULL;
+    if (FileUtil::fileExists((par.db3 + "_ca3m.ffdata").c_str())) {
+        resultAbReader = new DBReader<unsigned int>((par.db3 + "_ca3m.ffdata").c_str(), (par.db3 + "_ca3m.ffindex").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+        resultAbReader->open(DBReader<unsigned int>::LINEAR_ACCCESS);
+
+        cReader = new DBReader<unsigned int>((par.db3 + "_sequence.ffdata").c_str(), (par.db3 + "_sequence.ffindex").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+        cReader->open(DBReader<unsigned int>::SORT_BY_LINE);
+        isCa3m = true;
+    } else {
+        resultAbReader = new DBReader<unsigned int>(par.db3.c_str(), par.db3Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+        resultAbReader->open(DBReader<unsigned int>::LINEAR_ACCCESS);
+
+        cReader = new DBReader<unsigned int>(par.db2.c_str(), par.db2Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+        cReader->open(DBReader<unsigned int>::NOSORT);
+        if (par.preloadMode != Parameters::PRELOAD_MODE_MMAP) {
+            cReader->readMmapedDataInMemory();
+        }
     }
 
-    if (Parameters::isEqualDbtype(queryDbType, Parameters::DBTYPE_HMM_PROFILE) && Parameters::isEqualDbtype(targetDbType, Parameters::DBTYPE_HMM_PROFILE)) {
-        Debug(Debug::ERROR) << "Profile-profile is currently not supported.\n";
+    const int cSeqDbType = cReader->getDbtype();
+    if (Parameters::isEqualDbtype(aSeqDbType, Parameters::DBTYPE_HMM_PROFILE) && Parameters::isEqualDbtype(cSeqDbType, Parameters::DBTYPE_HMM_PROFILE)) {
+        Debug(Debug::ERROR) << "Profile-profile is currently not supported\n";
         return EXIT_FAILURE;
     }
 
-    DBReader<unsigned int> *resultReader = NULL;
-    DBReader<unsigned int> *ca3mSequenceReader = NULL;
-    if (FileUtil::fileExists((par.db3 + "_ca3m.ffdata").c_str())) {
-
-        resultReader = new DBReader<unsigned int>((par.db3 + "_ca3m.ffdata").c_str(), (par.db3 + "_ca3m.ffindex").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-        resultReader->open(DBReader<unsigned int>::LINEAR_ACCCESS);
-
-        ca3mSequenceReader = new DBReader<unsigned int>((par.db3 + "_sequence.ffdata").c_str(), (par.db3 + "_sequence.ffindex").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-        ca3mSequenceReader->open(DBReader<unsigned int>::SORT_BY_LINE);
-    } else {
-
-        resultReader = new DBReader<unsigned int>(par.db3.c_str(), par.db3Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-        resultReader->open(DBReader<unsigned int>::LINEAR_ACCCESS);
-    }
-
-    DBReader<unsigned int> expansionReader(par.db4.c_str(), par.db4Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-    expansionReader.open(DBReader<unsigned int>::NOSORT);
+    DBReader<unsigned int> resultBcReader(par.db4.c_str(), par.db4Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
+    resultBcReader.open(DBReader<unsigned int>::NOSORT);
     if (par.preloadMode != Parameters::PRELOAD_MODE_MMAP) {
-        expansionReader.readMmapedDataInMemory();
+        resultBcReader.readMmapedDataInMemory();
     }
 
-    Debug(Debug::INFO) << "Output database: " << par.db5 << "\n";
     DBWriter writer(par.db5.c_str(), par.db5Index.c_str(), par.threads, par.compressed, Parameters::DBTYPE_ALIGNMENT_RES);
     writer.open();
 
     BacktraceTranslator translator;
     SubstitutionMatrix subMat(par.scoringMatrixFile.aminoacids, 2.0, par.scoreBias);
-    EvalueComputation evaluer(targetReader.getAminoAcidDBSize(), &subMat, par.gapOpen.aminoacids, par.gapExtend.aminoacids);
-    Debug::Progress progress(resultReader->getSize());
-
-    Debug(Debug::INFO) << "Computing expanded alignment result...\n";
+    EvalueComputation evaluer(cReader->getAminoAcidDBSize(), &subMat, par.gapOpen.aminoacids, par.gapExtend.aminoacids);
+    Debug::Progress progress(resultAbReader->getSize());
 #pragma omp parallel
     {
         unsigned int thread_idx = 0;
 #ifdef OPENMP
         thread_idx = static_cast<unsigned int>(omp_get_thread_num());
 #endif
-        Sequence qSeq(par.maxSeqLen, queryDbType, &subMat, 0, false, par.compBiasCorrection);
-        Sequence tSeq(par.maxSeqLen, targetDbType, &subMat, 0, false, false);
-        float *compositionBias = new float[par.maxSeqLen + 1]();
 
-        std::vector<Matcher::result_t> expanded;
-        expanded.reserve(300);
+        Sequence aSeq(par.maxSeqLen, aSeqDbType, &subMat, 0, false, par.compBiasCorrection);
+        Sequence cSeq(par.maxSeqLen, cSeqDbType, &subMat, 0, false, false);
 
-        std::vector<Matcher::result_t> results;
-        results.reserve(1000);
+        size_t compBufferSize = (par.maxSeqLen + 1) * sizeof(float);
+        float *compositionBias = (float*)malloc(compBufferSize);
+        memset(compositionBias, 0, compBufferSize);
 
-        char buffer[1024];
+        char buffer[1024 + 32000];
 
-        Matcher::result_t resultAC;
-        resultAC.backtrace.reserve(par.maxSeqLen + 1);
+        std::vector<Matcher::result_t> resultsBc;
+        resultsBc.reserve(300);
+
+        Matcher::result_t resultAc;
+        resultAc.backtrace.reserve(par.maxSeqLen + 1);
+        Matcher::result_t currBestAc;
+
+        std::vector<Matcher::result_t> resultsAc;
+        resultsAc.reserve(1000);
 
 #pragma omp for schedule(dynamic, 10)
-        for (size_t i = 0; i < resultReader->getSize(); ++i) {
+        for (size_t i = 0; i < resultAbReader->getSize(); ++i) {
             progress.updateProgress();
-            unsigned int queryKey = resultReader->getDbKey(i);
 
-            size_t querySeqId = queryReader.getId(queryKey);
-            qSeq.mapSequence(querySeqId, queryKey, queryReader.getData(querySeqId, thread_idx),
-                             queryReader.getSeqLen(querySeqId));
+            unsigned int resAbKey = resultAbReader->getDbKey(i);
 
-            if(par.compBiasCorrection == true && Parameters::isEqualDbtype(queryDbType,Parameters::DBTYPE_AMINO_ACIDS)){
-                SubstitutionMatrix::calcLocalAaBiasCorrection(&subMat, qSeq.numSequence, qSeq.L, compositionBias);
+            size_t aSeqId = aReader.getId(resAbKey);
+            aSeq.mapSequence(aSeqId, resAbKey, aReader.getData(aSeqId, thread_idx), aReader.getSeqLen(aSeqId));
+
+            if (par.compBiasCorrection == true && Parameters::isEqualDbtype(aSeqDbType, Parameters::DBTYPE_AMINO_ACIDS)) {
+                if ((size_t)aSeq.L >= compBufferSize) {
+                    compBufferSize = (size_t)aSeq.L * 1.5 * sizeof(float);
+                    compositionBias = (float*)realloc(compositionBias, compBufferSize);
+                    memset(compositionBias, 0, compBufferSize);
+                }
+                SubstitutionMatrix::calcLocalAaBiasCorrection(&subMat, aSeq.numSequence, aSeq.L, compositionBias);
             }
 
-            char *data = resultReader->getData(i, thread_idx);
+            char *data = resultAbReader->getData(i, thread_idx);
             while (*data != '\0') {
-                Matcher::result_t resultAB = Matcher::parseAlignmentRecord(data, false);
+                Matcher::result_t resultAb = Matcher::parseAlignmentRecord(data, false);
+                data = Util::skipLine(data);
 
-                if (resultAB.backtrace.empty()) {
-                    Debug(Debug::ERROR) << "Alignment must contain a backtrace.\n";
+                if (resultAb.backtrace.empty()) {
+                    Debug(Debug::ERROR) << "Alignment must contain a backtrace\n";
                     EXIT(EXIT_FAILURE);
                 }
-//                Matcher::resultToBuffer(buffer, resultAB, true, true);
-//                Debug(Debug::INFO) << buffer;
 
-                unsigned int targetKey = resultAB.dbKey;
-                size_t targetId = expansionReader.getId(targetKey);
-
-                if (ca3mSequenceReader != NULL) {
+                unsigned int bResKey = resultAb.dbKey;
+                size_t bResId = resultBcReader.getId(bResKey);
+                if (isCa3m) {
                     unsigned int key;
-                    CompressedA3M::extractMatcherResults(key, expanded, expansionReader.getData(targetId, thread_idx),
-                                                         expansionReader.getEntryLen(targetId), *ca3mSequenceReader, false);
+                    CompressedA3M::extractMatcherResults(key, resultsBc, resultBcReader.getData(bResId, thread_idx),
+                                                         resultBcReader.getEntryLen(bResId), *cReader, false);
                 } else {
-                    Matcher::readAlignmentResults(expanded, expansionReader.getData(targetId, thread_idx), false);
+                    Matcher::readAlignmentResults(resultsBc, resultBcReader.getData(bResId, thread_idx), false);
                 }
-                for (size_t k = 0; k < expanded.size(); ++k) {
-                    Matcher::result_t &resultBC = expanded[k];
 
-                    if (resultBC.backtrace.empty()) {
-                        Debug(Debug::ERROR) << "Alignment must contain a backtrace.\n";
+                std::stable_sort(resultsAc.begin(), resultsAc.end(), compareHitsByKeyScore);
+
+                size_t lastCKey = SIZE_MAX;
+                currBestAc.score = INT_MIN;
+                for (size_t k = 0; k < resultsBc.size(); ++k) {
+                    Matcher::result_t &resultBc = resultsBc[k];
+                    if (resultBc.backtrace.size() == 0) {
+                        Debug(Debug::ERROR) << "Alignment must contain a backtrace\n";
                         EXIT(EXIT_FAILURE);
                     }
-//                    Matcher::resultToBuffer(buffer, resultBC, true, true);
-//                    Debug(Debug::INFO) << buffer;
-
-                    translator.translateResult(resultAB, resultBC, resultAC);
-                    if (resultAC.backtrace.empty()) {
+                    translator.translateResult(resultAb, resultBc, resultAc);
+                    if (resultAc.backtrace.size() == 0) {
                         continue;
                     }
 
-                    if (Util::canBeCovered(par.covThr, par.covMode, resultAC.qLen, resultAC.dbLen) == false) {
+                    if (Util::canBeCovered(par.covThr, par.covMode, resultAc.qLen, resultAc.dbLen) == false) {
                         continue;
                     }
-                    size_t bcTargetSeqId = targetReader.getId(resultBC.dbKey);
-                    tSeq.mapSequence(bcTargetSeqId, targetKey, targetReader.getData(bcTargetSeqId, thread_idx),
-                                     targetReader.getSeqLen(bcTargetSeqId));
 
-                    rescoreResultByBacktrace(resultAC, qSeq, tSeq, subMat, compositionBias,
-                                             evaluer, par.gapOpen.aminoacids, par.gapExtend.aminoacids, par.seqIdMode);
+                    unsigned int cSeqKey = resultBc.dbKey;
+                    if (lastCKey != cSeqKey) {
+                        if (currBestAc.score != INT_MIN) {
+                            resultsAc.emplace_back(currBestAc);
+                        }
+                        currBestAc.score = INT_MIN;
 
-                    if (Alignment::checkCriteria(resultAC, false, par.evalThr, par.seqIdThr, par.alnLenThr, par.covMode, par.covThr)) {
-                        results.emplace_back(resultAC);
+                        size_t cSeqId = cReader->getId(cSeqKey);
+                        cSeq.mapSequence(cSeqId, cSeqKey, cReader->getData(cSeqId, thread_idx), cReader->getSeqLen(cSeqId));
+                        lastCKey = cSeqKey;
+                    }
+                    rescoreResultByBacktrace(resultAc, aSeq, cSeq, subMat, compositionBias, par.gapOpen.aminoacids, par.gapExtend.aminoacids);
+                    if (resultAc.score > par.minDiagScoreThr && resultAc.score > currBestAc.score) {
+                        currBestAc = resultAc;
                     }
                 }
-                expanded.clear();
-                data = Util::skipLine(data);
-            }
-
-            std::vector<Matcher::result_t> *finalResults = &results;
-            if (par.expansionMode == 1) {
-                // keep only the best hit to same target
-                SORT_SERIAL(results.begin(), results.end(), compareHitsByKeyEvalScore);
-                ssize_t lastKey = -1;
-                for (size_t j = 0; j < results.size(); ++j) {
-                    const Matcher::result_t& res = results[j];
-                    if (res.dbKey != lastKey) {
-                        expanded.emplace_back(res);
-                    }
-                    lastKey = res.dbKey;
+                if (currBestAc.score != INT_MIN) {
+                    resultsAc.emplace_back(currBestAc);
                 }
-                finalResults = &expanded;
+                resultsBc.clear();
             }
 
-            SORT_SERIAL(finalResults->begin(), finalResults->end(), Matcher::compareHits);
-
+            SORT_SERIAL(resultsAc.begin(), resultsAc.end(), Matcher::compareHits);
             writer.writeStart(thread_idx);
-            for (size_t j = 0; j < finalResults->size(); ++j) {
-                size_t len = Matcher::resultToBuffer(buffer, (*finalResults)[j], true, true);
+            for (size_t j = 0; j < resultsAc.size(); ++j) {
+                size_t len = Matcher::resultToBuffer(buffer, resultsAc[j], true, true);
                 writer.writeAdd(buffer, len, thread_idx);
             }
-            writer.writeEnd(queryKey, thread_idx);
-            expanded.clear();
-            results.clear();
+            writer.writeEnd(resAbKey, thread_idx);
+            resultsAc.clear();
         }
-
-        delete[] compositionBias;
+        free(compositionBias);
     }
-    Debug(Debug::INFO) << "\n";
     writer.close();
-    expansionReader.close();
-    resultReader->close();
-    delete resultReader;
-    if (ca3mSequenceReader != NULL) {
-        ca3mSequenceReader->close();
-        delete ca3mSequenceReader;
+    resultBcReader.close();
+    resultAbReader->close();
+    delete resultAbReader;
+    if (cReader != NULL) {
+        cReader->close();
+        delete cReader;
     }
-    targetReader.close();
-    queryReader.close();
+    aReader.close();
 
     return EXIT_SUCCESS;
 }
