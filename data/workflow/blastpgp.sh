d4dd06d27a512fddf9e834d868356b5d529e9ec2
Milot Mirdita <milot@mirdita.de>
2021-06-19 00:01:48 +0200

    Fix iterative profile search restartable again

--- data/workflow/blastpgp.sh
+++ data/workflow/blastpgp.sh
@@ -27,7 +27,7 @@ STEP=0
 [ -z "$NUM_IT" ] && NUM_IT=3;
 while [ $STEP -lt $NUM_IT ]; do
     # call prefilter module
-    if notExists "$TMP_PATH/pref_$STEP.dbtype"; then
+    if notExists "$TMP_PATH/pref_tmp_${STEP}.done"; then
         PARAM="PREFILTER_PAR_$STEP"
         eval TMP="\$$PARAM"
         if [ $STEP -eq 0 ]; then
@@ -39,24 +39,22 @@ while [ $STEP -lt $NUM_IT ]; do
             $RUNNER "$MMSEQS" prefilter "$QUERYDB" "$2" "$TMP_PATH/pref_tmp_$STEP" ${TMP} \
                 || fail "Prefilter died"
         fi
+        touch "$TMP_PATH/pref_tmp_${STEP}.done"
     fi
 
     if [ $STEP -ge 1 ]; then
-        if notExists "$TMP_PATH/pref_$STEP.dbtype"; then
+        if notExists "$TMP_PATH/pref_$STEP.done"; then
             STEPONE=$((STEP-1))
             # shellcheck disable=SC2086
             "$MMSEQS" subtractdbs "$TMP_PATH/pref_tmp_$STEP" "$TMP_PATH/aln_$STEPONE" "$TMP_PATH/pref_$STEP" $SUBSTRACT_PAR \
-            || fail "Substract died"
-
+                || fail "Substract died"
             "$MMSEQS" rmdb "$TMP_PATH/pref_tmp_$STEP"
-
-            #mv -f "$TMP_PATH/pref_next_$STEP" "$TMP_PATH/pref_$STEP"
-            #mv -f "$TMP_PATH/pref_next_$STEP.index" "$TMP_PATH/pref_$STEP.index"
         fi
+        touch "$TMP_PATH/pref_$STEP.done"
     fi
 
 	# call alignment module
-	if notExists "$TMP_PATH/aln_tmp_$STEP.dbtype"; then
+	if notExists "$TMP_PATH/aln_tmp_$STEP.done"; then
 	    PARAM="ALIGNMENT_PAR_$STEP"
         eval TMP="\$$PARAM"
 
@@ -69,21 +67,22 @@ while [ $STEP -lt $NUM_IT ]; do
             $RUNNER "$MMSEQS" "${ALIGN_MODULE}" "$QUERYDB" "$2" "$TMP_PATH/pref_$STEP" "$TMP_PATH/aln_tmp_$STEP" ${TMP} \
                 || fail "Alignment died"
         fi
+        touch "$TMP_PATH/aln_tmp_$STEP.done"
     fi
 
     if [ $STEP -gt 0 ]; then
-        if notExists "$TMP_PATH/aln_$STEP.dbtype"; then
+        if notExists "$TMP_PATH/aln_$STEP.done"; then
             STEPONE=$((STEP-1))
-
             if [ $STEP -ne $((NUM_IT  - 1)) ]; then
                 "$MMSEQS" mergedbs "$QUERYDB" "$TMP_PATH/aln_$STEP" "$TMP_PATH/aln_$STEPONE" "$TMP_PATH/aln_tmp_$STEP" \
                     || fail "Alignment died"
             else
                 "$MMSEQS" mergedbs "$QUERYDB" "$3" "$TMP_PATH/aln_$STEPONE" "$TMP_PATH/aln_tmp_$STEP" \
-                        || fail "Alignment died"
+                    || fail "Alignment died"
             fi
             "$MMSEQS" rmdb "$TMP_PATH/aln_$STEPONE"
             "$MMSEQS" rmdb "$TMP_PATH/aln_tmp_$STEP"
+            touch "$TMP_PATH/aln_$STEP.done"
         fi
     fi
 
@@ -94,7 +93,7 @@ while [ $STEP -lt $NUM_IT ]; do
             eval TMP="\$$PARAM"
             # shellcheck disable=SC2086
             $RUNNER "$MMSEQS" result2profile "$QUERYDB" "$2" "$TMP_PATH/aln_$STEP" "$TMP_PATH/profile_$STEP" ${TMP} \
-            || fail "Create profile died"
+                || fail "Create profile died"
         fi
     fi
 	QUERYDB="$TMP_PATH/profile_$STEP"
@@ -104,12 +103,18 @@ done
 if [ -n "$REMOVE_TMP" ]; then
     STEP=0
     while [ "$STEP" -lt "$NUM_IT" ]; do
+        if [ $STEP -gt 0 ]; then
+            rm -f -- "$TMP_PATH/aln_$STEP.done" "$TMP_PATH/pref_$STEP.done"
+        fi
+        rm -f -- "$TMP_PATH/aln_tmp_$STEP.done" "$TMP_PATH/pref_tmp_${STEP}.done"
         # shellcheck disable=SC2086
         "$MMSEQS" rmdb "${TMP_PATH}/pref_$STEP" ${VERBOSITY}
         # shellcheck disable=SC2086
         "$MMSEQS" rmdb "${TMP_PATH}/aln_$STEP" ${VERBOSITY}
         # shellcheck disable=SC2086
         "$MMSEQS" rmdb "${TMP_PATH}/profile_$STEP" ${VERBOSITY}
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/profile_${STEP}_h" ${VERBOSITY}
         STEP=$((STEP+1))
     done
     rm -f "$TMP_PATH/blastpgp.sh"
