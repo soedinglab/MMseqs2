fad2d1865a7a52457f9ddb5dc206f86baba315e3
Milot Mirdita <milot@mirdita.de>
2021-07-02 18:40:05 +0200

    Solve a few multithreading issues in gff2db

--- src/util/gff2db.cpp
+++ src/util/gff2db.cpp
@@ -14,12 +14,10 @@ int gff2db(int argc, const char **argv, const Command &command) {
     Parameters &par = Parameters::getInstance();
     par.parseParameters(argc, argv, command, true, Parameters::PARSE_VARIADIC, 0);
 
-    std::vector<std::string> filenames(par.filenames);
     std::string outDb = par.filenames.back();
     par.filenames.pop_back();
     std::string seqDb = par.filenames.back();
     par.filenames.pop_back();
-
 
     DBReader<unsigned int> reader(seqDb.c_str(), (seqDb + ".index").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA | DBReader<unsigned int>::USE_LOOKUP_REV);
     reader.open(DBReader<unsigned int>::NOSORT);
@@ -30,32 +28,37 @@ int gff2db(int argc, const char **argv, const Command &command) {
     writer.open();
     DBWriter headerWriter((outDb + "_h").c_str(), (outDb + "_h.index").c_str(), par.threads, par.compressed, Parameters::DBTYPE_GENERIC_DB);
     headerWriter.open();
+    DBWriter lookupWriter((outDb + ".lookup").c_str(), (outDb + ".lookup.index").c_str(), par.threads, 0, Parameters::DBTYPE_OMIT_FILE);
+    lookupWriter.open();
 
-    FILE *lookup = FileUtil::openAndDelete((outDb + ".lookup").c_str(), "w");
     FILE *source = FileUtil::openAndDelete((outDb + ".source").c_str(), "w");
-
-    bool shouldCompareType = par.gffType.length() > 0;
-
-    if (par.filenames.size() < reader.getSize()) {
-        Debug(Debug::WARNING) << "Not enough GFF files are provided. Some results might be omitted. \n";
-        //EXIT(EXIT_FAILURE);
+    for (size_t i = 0; i < par.filenames.size(); ++i) {
+        if (fprintf(source, "%zu\t%s\n", i, FileUtil::baseName(par.filenames[i]).c_str()) < 0) {
+            Debug(Debug::ERROR) << "Cannot write to file " << outDb << ".source\n";
+            EXIT(EXIT_FAILURE);
+        }
+    }
+    if (fclose(source) != 0) {
+        Debug(Debug::ERROR) << "Cannot close file " << outDb << ".source\n";
+        EXIT(EXIT_FAILURE);
     }
 
+    bool shouldCompareType = par.gffType.length() > 0;
+    if (par.filenames.size() < reader.getSize()) {
+        Debug(Debug::WARNING) << "Not enough GFF files are provided. Some results might be omitted. \n";
+    }
+
+    unsigned int entries_num = 0;
     Debug::Progress progress(par.filenames.size());
 #pragma omp parallel
     {
         int thread_idx = 0;
-        
 #ifdef OPENMP
         thread_idx = omp_get_thread_num();
 #endif
 
-        unsigned int entries_num = 0;
-        char buffer[1024];
+        char buffer[32768];
         const char* fields[255];
-
-        std::string filename;
-        filename.reserve(100);
 
         std::string revStr;
         revStr.reserve(par.maxSeqLen + 1);
@@ -63,15 +66,13 @@ int gff2db(int argc, const char **argv, const Command &command) {
 #pragma omp for schedule(dynamic, 10)
         for (size_t i = 0; i < par.filenames.size(); ++i) {
             progress.updateProgress();
-            filename = par.filenames[i];
-            MemoryMapped file(filename, MemoryMapped::WholeFile, MemoryMapped::SequentialScan);
+            MemoryMapped file(par.filenames[i], MemoryMapped::WholeFile, MemoryMapped::SequentialScan);
             if (!file.isValid()) {
-                Debug(Debug::ERROR) << "Could not open GFF file " << filename << "\n";
+                Debug(Debug::ERROR) << "Could not open GFF file " << par.filenames[i] << "\n";
                 EXIT(EXIT_FAILURE);
             }
             char *data = (char *) file.getData();
             size_t idx = 0;
-            fprintf(source, "%zu\t%s\n", i, filename.c_str());
             while (*data != '\0') {
                 // line is a comment
                 if (*data == '#') {
@@ -93,10 +94,11 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     }
                 }
 
+                unsigned int key = __sync_fetch_and_add(&(entries_num), 1);
                 size_t start = Util::fast_atoi<size_t>(fields[3]);
                 size_t end = Util::fast_atoi<size_t>(fields[4]);
                 if (start == end) {
-                    Debug(Debug::WARNING) << "Invalid sequence length in line " << entries_num << "!\n";
+                    Debug(Debug::WARNING) << "Invalid sequence length in line " << key << "!\n";
                     continue;
                 }
                 std::string strand(fields[6], fields[7] - fields[6] - 1);
@@ -106,25 +108,19 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     Debug(Debug::ERROR) << "GFF entry not found in database lookup: " << name << "!\n";
                     EXIT(EXIT_FAILURE);
                 }
-                unsigned int key = reader.getLookupKey(lookupId);
-
-                size_t headerId = headerReader.getId(key);
-                if (headerId == UINT_MAX) {
-                    Debug(Debug::ERROR) << "GFF entry not found in header database: " << name << "!\n";
-                    EXIT(EXIT_FAILURE);
-                }
-                unsigned int id = par.identifierOffset + entries_num;
+                unsigned int lookupKey = reader.getLookupKey(lookupId);
 
                 idx++;
+                size_t bufferLen;
                 if (strand == "+") {
-                    Orf::writeOrfHeader(buffer, key, start, end, 0, 0);
+                    bufferLen = Orf::writeOrfHeader(buffer, lookupKey, start, end, 0, 0);
                 } else {
-                    Orf::writeOrfHeader(buffer, key, end, start, 0, 0);
+                    bufferLen = Orf::writeOrfHeader(buffer, lookupKey, end, start, 0, 0);
                 }
-                headerWriter.writeData(buffer, strlen(buffer), key, thread_idx);
+                headerWriter.writeData(buffer, bufferLen, key, thread_idx);
                 
 
-                size_t seqId = reader.getId(key);
+                size_t seqId = reader.getId(lookupKey);
                 if (seqId == UINT_MAX) {
                     Debug(Debug::ERROR) << "GFF entry not found in sequence database: " << name << "!\n";
                     EXIT(EXIT_FAILURE);
@@ -136,29 +132,29 @@ int gff2db(int argc, const char **argv, const Command &command) {
 
                 writer.writeStart(thread_idx);
                 if (strand == "+") {
-                    fprintf(lookup, "%u\t%s_%zu_%zu_%zu\t%zu\n", id, name.c_str(), idx, start, end, i);
+                    size_t len = snprintf(buffer, sizeof(buffer), "%u\t%s_%zu_%zu_%zu\t%zu\n", key, name.c_str(), idx, start, end, i);
+                    lookupWriter.writeData(buffer, len, thread_idx, false, false);
                     writer.writeAdd(seq + start - 1 , length, thread_idx);
                 } else {
-                    fprintf(lookup, "%u\t%s_%zu_%zu_%zu\t%zu\n", id, name.c_str(), idx, end, start, i);
+                    size_t len = snprintf(buffer, sizeof(buffer), "%u\t%s_%zu_%zu_%zu\t%zu\n", key, name.c_str(), idx, end, start, i);
+                    lookupWriter.writeData(buffer, len, thread_idx, false, false);
                     for (size_t i = end - 1 ; i >= end - length; i--) {
                         revStr.append(1, Orf::complement(seq[i]));
                     }
-                if (revStr.size() % 3 != 0) {
-                    Debug(Debug::WARNING) << "The nucleotide sequence length is not divisible by 3!\n";
-                }  
+                    if (revStr.size() % 3 != 0) {
+                        Debug(Debug::WARNING) << "The nucleotide sequence length is not divisible by 3!\n";
+                    }
                     writer.writeAdd(revStr.c_str(), revStr.size(), thread_idx);
                     revStr.clear();
                 }
                 writer.writeAdd("\n", 1, thread_idx);
-                writer.writeEnd(id, thread_idx);
-
-                entries_num++;
+                writer.writeEnd(key, thread_idx);
             }
             file.close();
         }
     }
-    fclose(lookup);
-    fclose(source);
+    lookupWriter.close(true);
+    FileUtil::remove(lookupWriter.getIndexFileName());
     headerWriter.close(true);
     writer.close(true);
     headerReader.close();
