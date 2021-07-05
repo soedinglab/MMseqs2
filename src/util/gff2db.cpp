f3b0cc33be654aa5b6fa89a74947a64c4508e03a
Milot Mirdita <milot@mirdita.de>
2021-07-05 16:09:34 +0200

    General gff2db cleanup

--- src/util/gff2db.cpp
+++ src/util/gff2db.cpp
@@ -24,23 +24,28 @@ int gff2db(int argc, const char **argv, const Command &command) {
     DBReader<unsigned int> headerReader((seqDb + "_h").c_str(), (seqDb + "_h.index").c_str(), par.threads, DBReader<unsigned int>::USE_INDEX | DBReader<unsigned int>::USE_DATA);
     headerReader.open(DBReader<unsigned int>::NOSORT);
 
-    DBWriter writer(outDb.c_str(), (outDb + ".index").c_str(), par.threads, par.compressed, Parameters::DBTYPE_NUCLEOTIDES);
+    std::string outDbIndex = outDb + ".index";
+    DBWriter writer(outDb.c_str(), outDbIndex.c_str(), par.threads, par.compressed, Parameters::DBTYPE_NUCLEOTIDES);
     writer.open();
-    DBWriter headerWriter((outDb + "_h").c_str(), (outDb + "_h.index").c_str(), par.threads, par.compressed, Parameters::DBTYPE_GENERIC_DB);
+    std::string outHdr = outDb + "_h";
+    std::string outHdrIndex = outDb + "_h.index";
+    DBWriter headerWriter(outHdr.c_str(), outHdrIndex.c_str(), par.threads, par.compressed, Parameters::DBTYPE_GENERIC_DB);
     headerWriter.open();
-    DBWriter lookupWriter((outDb + ".lookup").c_str(), (outDb + ".lookup.index").c_str(), par.threads, 0, Parameters::DBTYPE_OMIT_FILE);
+    std::string outLookup = outDb + ".lookup";
+    std::string outLookupIndex = outDb + ".lookup.index";
+    DBWriter lookupWriter(outLookup.c_str(), outLookupIndex.c_str(), par.threads, 0, Parameters::DBTYPE_OMIT_FILE);
     lookupWriter.open();
 
     FILE *source = FileUtil::openAndDelete((outDb + ".source").c_str(), "w");
     for (size_t i = 0; i < par.filenames.size(); ++i) {
         if (fprintf(source, "%zu\t%s\n", i, FileUtil::baseName(par.filenames[i]).c_str()) < 0) {
             Debug(Debug::ERROR) << "Cannot write to file " << outDb << ".source\n";
-            EXIT(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
     }
     if (fclose(source) != 0) {
         Debug(Debug::ERROR) << "Cannot close file " << outDb << ".source\n";
-        EXIT(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     const std::vector<std::string> features = Util::split(par.gffType, ",");
@@ -78,8 +83,8 @@ int gff2db(int argc, const char **argv, const Command &command) {
             char *data = (char *) file.getData();
             size_t idx = 0;
             while (*data != '\0') {
-                // line is a comment
-                if (*data == '#') {
+                // line is a comment or empty
+                if (*data == '#' || *data == '\n') {
                     data = Util::skipLine(data);
                     continue;
                 }
@@ -103,11 +108,10 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     continue;
                 }
                 cont:
-                unsigned int key = __sync_fetch_and_add(&(entries_num), 1);
                 size_t start = Util::fast_atoi<size_t>(fields[3]);
                 size_t end = Util::fast_atoi<size_t>(fields[4]);
                 if (start == end) {
-                    Debug(Debug::WARNING) << "Invalid sequence length in line " << key << "!\n";
+                    Debug(Debug::WARNING) << "Invalid sequence length in line " << idx << "!\n";
                     continue;
                 }
                 std::string strand(fields[6], fields[7] - fields[6] - 1);
@@ -118,8 +122,13 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     EXIT(EXIT_FAILURE);
                 }
                 unsigned int lookupKey = reader.getLookupKey(lookupId);
+                size_t seqId = reader.getId(lookupKey);
+                if (seqId == UINT_MAX) {
+                    Debug(Debug::ERROR) << "GFF entry not found in sequence database: " << name << "!\n";
+                    EXIT(EXIT_FAILURE);
+                }
 
-                idx++;
+                unsigned int key = __sync_fetch_and_add(&(entries_num), 1);
                 size_t bufferLen;
                 if (strand == "+") {
                     bufferLen = Orf::writeOrfHeader(buffer, lookupKey, start, end, 0, 0);
@@ -127,18 +136,10 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     bufferLen = Orf::writeOrfHeader(buffer, lookupKey, end, start, 0, 0);
                 }
                 headerWriter.writeData(buffer, bufferLen, key, thread_idx);
-                
 
-                size_t seqId = reader.getId(lookupKey);
-                if (seqId == UINT_MAX) {
-                    Debug(Debug::ERROR) << "GFF entry not found in sequence database: " << name << "!\n";
-                    EXIT(EXIT_FAILURE);
-                }
-
+                char *seq = reader.getData(seqId, thread_idx);
                 //check the strand instead of end - start 
                 ssize_t length = end - start + 1;
-                char *seq = reader.getData(seqId, thread_idx);
-
                 writer.writeStart(thread_idx);
                 if (strand == "+") {
                     size_t len = snprintf(buffer, sizeof(buffer), "%u\t%s_%zu_%zu_%zu\t%zu\n", key, name.c_str(), idx, start, end, i);
@@ -150,14 +151,12 @@ int gff2db(int argc, const char **argv, const Command &command) {
                     for (size_t i = end - 1 ; i >= end - length; i--) {
                         revStr.append(1, Orf::complement(seq[i]));
                     }
-                    if (revStr.size() % 3 != 0) {
-                        Debug(Debug::WARNING) << "The nucleotide sequence length is not divisible by 3!\n";
-                    }
                     writer.writeAdd(revStr.c_str(), revStr.size(), thread_idx);
                     revStr.clear();
                 }
                 writer.writeAdd("\n", 1, thread_idx);
                 writer.writeEnd(key, thread_idx);
+                idx++;
             }
             file.close();
         }
