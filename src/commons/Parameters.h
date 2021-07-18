31eb67ae0eadea8d639c80de019a8fea5033cb09
Martin Steinegger <themartinsteinegger@gmail.com>
2021-07-18 22:36:23 +0900

    Add A3M support to result2msa

--- src/commons/Parameters.h
+++ src/commons/Parameters.h
@@ -129,7 +129,7 @@ public:
     static const int FORMAT_MSA_FASTADB = 2;
     static const int FORMAT_MSA_FASTADB_SUMMARY = 3;
     static const int FORMAT_MSA_STOCKHOLM_FLAT = 4;
-
+    static const int FORMAT_MSA_A3M = 5;
     // outfmt
     static const int OUTFMT_QUERY = 0;
     static const int OUTFMT_TARGET = 1;
