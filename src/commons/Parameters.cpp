c9107bacbaf18ec9d2526cbd9b6de7dd4d64fbc5
Martin Steinegger <themartinsteinegger@gmail.com>
2025-07-20 16:50:19 +0900

    Add proximity parameter to pairaln list

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -288,8 +288,7 @@ Parameters::Parameters():
         PARAM_PAIRING_DUMMY_MODE(PARAM_PAIRING_DUMMY_MODE_ID, "--pairing-dummy-mode", "Include dummy pairing", "0: dont include, 1: include - an entry that will cause result2msa to write a gap only line", typeid(int), (void *) &pairdummymode, "^[0-1]{1}$", MMseqsParameter::COMMAND_EXPERT),
         PARAM_PAIRING_MODE(PARAM_PAIRING_MODE_ID, "--pairing-mode", "Pairing mode", "0: pair maximal per species, 1: pair only if all chains are covered per species", typeid(int), (void *) &pairmode, "^[0-1]{1}$", MMseqsParameter::COMMAND_EXPERT),
         PARAM_PAIRING_FILTER(PARAM_PAIRING_FILTER_ID, "--pairing-filter", "Pairing filter", "filter hits by 0: top hit, 1: pair by proximity of IDs", typeid(int), (void *) &pairfilter, "^[0-1]{1}$", MMseqsParameter::COMMAND_EXPERT),
-        PARAM_PAIRING_PROX_DISTANCE(PARAM_PAIRING_PROX_DISTANCE_ID, "--pairing-prox-distance", "Proximity distance threshold", "adjust the distance threshold for pairing (--pairing-filter 1)", typeid(int), (void *) &pairProximityDistance, "^[0-9]{1}[0-9]*$", MMseqsParameter::COMMAND_EXPERT),
-
+        PARAM_PAIRING_PROX_DIST(PARAM_PAIRING_PROX_DIST_ID, "--pairing-prox-dist", "Proximity distance threshold", "adjust the distance threshold for pairing (--pairing-filter 1)", typeid(int), (void *) &pairProximityDistance, "^[0-9]{1}[0-9]*$", MMseqsParameter::COMMAND_EXPERT),
         // taxonomyreport
         PARAM_REPORT_MODE(PARAM_REPORT_MODE_ID, "--report-mode", "Report mode", "Taxonomy report mode\n0: Kraken\n1: Krona\n2: do not create a report (for workflows only)\n3: Kraken per query database", typeid(int), (void *) &reportMode, "^[0-3]{1}$"),
         // createtaxdb
@@ -1289,6 +1288,7 @@ Parameters::Parameters():
     pairaln.push_back(&PARAM_PAIRING_DUMMY_MODE);
     pairaln.push_back(&PARAM_PAIRING_MODE);
     pairaln.push_back(&PARAM_PAIRING_FILTER);
+    pairaln.push_back(&PARAM_PAIRING_PROX_DIST);
     pairaln.push_back(&PARAM_COMPRESSED);
     pairaln.push_back(&PARAM_THREADS);
     pairaln.push_back(&PARAM_V);
