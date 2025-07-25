e7ff4c7dc121da5a5cccbec7098a2a257adf74c7
Milot Mirdita <milot@mirdita.de>
2025-07-25 14:44:24 +0900

    Style

--- src/commons/LambdaCalculation.h
+++ src/commons/LambdaCalculation.h
@@ -2,16 +2,6 @@
 #define LAMBDA_CALC_H
 #include <vector>
 
-// Enum to specify matrix units
-enum class MatrixUnits {
-    HALF_BIT,    // Typical for BLOSUM matrices
-    BIT,         // Full bit units
-    NATURAL_LOG  // Natural log units (required for lambda calculation)
-};
-
-// Forward declaration
-struct Matrix;
-
 double calculate_lambda(
     const double** raw_mat_b,
     const int alpha_size,
